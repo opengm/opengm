51c574a5e147c84d0f6f07d4ac553bcb7b10f608
tbeier <tbeier@tbeier-machine.(none)>
2012-11-25 23:11:38 +0100

    cleaning

--- include/opengm/utilities/value_convert.hxx
+++ include/opengm/utilities/value_convert.hxx
@@ -16,20 +16,39 @@ namespace opengm{
    typedef SemiRing<Multiplier,Maximizer> MaxProd;
 
    template<class IN,class OUT,class VOUT>
-   struct ValueConverter;
-   
+   struct ValueConverter{
+	  template< class T>
+      static VOUT convert(const T in){
+		 throw RuntimeError("The requested semi-ring conversion not supported");
+         return 0;
+      }
+   };
+   // no conversion
    template <class INOUT,class VOUT>
    struct ValueConverter<INOUT,INOUT,VOUT> {
       template< class T>
       static VOUT convert(const T in){
          return in;
       }
-   }
+   };
+   // maxprod -> minsum
+   template <class VOUT>
+   struct ValueConverter<MaxProd,MinSum,VOUT> {
+      template< class T>
+      static VOUT convert(const T in){
+		 if(in <= 0.0 ) {
+			std::cout<<"Error,value is "<<in<<"\n";
+			throw RuntimeError("ere");
+		 }
+         return static_cast<VOUT>(-1.0)*std::log(static_cast<VOUT>(in));
+      }
+   };
+   // maxprod -> maxsum
    template <class VOUT>
    struct ValueConverter<MaxProd,MaxSum,VOUT> {
       template< class T>
       static VOUT convert(const T in){
          return std::log(static_cast<VOUT>(in));
       }
-   }
+   };
 }
