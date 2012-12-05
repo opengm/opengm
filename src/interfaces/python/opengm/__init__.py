d44c4bf5c36cc4e92a8d0fe9387905efecfdcbb6
tbeier <tbeier@tbeier-machine.(none)>
2012-12-05 18:22:03 +0100

    Minor bugfixed and minor improvments - Fixed bug in __init__.py (where maximization was not possible) - implemented factory function to easy get potts functions and squared differences functions

--- src/interfaces/python/opengm/__init__.py
+++ src/interfaces/python/opengm/__init__.py
@@ -21,14 +21,13 @@ def graphicalModel(numberOfLabels,operator='adder'):
    
    numberOfLabels -- number of label sequence (can be a list or  a 1d numpy.ndarray)
    
-   operator -- operator of the graphical model. Can be 'adder' or 'multiplier' (default 'adder')
+   operator -- operator of the graphical model. Can be 'adder' or 'multiplier' (default: 'adder')
    
    Construct a gm with ``\'adder\'`` as operator::
    
       gm=opengm.graphicalModel([2,2,2,2,2],operator='adder')
       # or just
       gm=opengm.graphicalModel([2,2,2,2,2])
-      # or just
       
    Construct a gm with ``\'multiplier\'`` as operator::  
    
@@ -43,8 +42,26 @@ def graphicalModel(numberOfLabels,operator='adder'):
       raise NameError('operator must be \'adder\' or \'multiplier\'') 
 
 
-
 def inferenceParameter(gm ,alg, accumulator=None):
+   """Factory function to construct a parameter object for an inference object.
+
+   Keyword arguments:
+   
+   gm -- a graphical model
+   
+   alg -- the inference algorithm ('bp','trbp','icm','astar','lf','a-expansion','ab-swap','graphcut',Lazyflipper',...)
+   
+   accumulator -- accumulator ('minimizer','maximizer') (default: depends on gm operator)
+      If the operator of the gm is ``\'adder\'`` the default
+      accumulator is ``\'minimizer\'`` .
+      If the operator of the gm is ``\'multiplier\'`` the default
+      accumulator is ``\'maximizer\'`` .
+
+   Construct a parameter object::
+   
+      #TODO
+      
+   """
    alg=alg.lower()
    #evaluate accumulator
    operator=gm.operator
@@ -54,17 +71,17 @@ def inferenceParameter(gm ,alg, accumulator=None):
       elif operator=='multiplier':
          accumulator='maximizer'
       else :
-         print operator
+         print "selected operator : ", operator
          raise NameError('operator must be \'adder\' or \'multiplier\'') 
    else:
       accumulator=accumulator.lower()
-      if accumulator is 'min' or 'minimizer':
+      if accumulator == 'min' or accumulator =='minimizer':
          accumulator='minimizer'
-      elif accumulator is 'max' or 'maximizer':
+      elif accumulator == 'max' or accumulator=='maximizer':
          accumulator='maximizer'
       else :
-         print operator
-         raise NameError('operator must be \'minimizer\' or \'maximizer\' (or just \'min\' or \'max\' )')   
+         print "selected accumulator: ", accumulator
+         raise NameError('accumulator must be \'minimizer\' or \'maximizer\' (or just \'min\' or \'max\' )')   
    #evaluate inference algorithm      
    name = 'inference.'+operator+'.'+accumulator
    #dirty hack!
@@ -102,9 +119,28 @@ def inferenceParameter(gm ,alg, accumulator=None):
    else:
       raise NameError( 'alg \'' + alg + '\' is unknown') 
 
-        
-        
-def inferenceAlgorithm(gm ,alg, accumulator=None,parameter=None):
+         
+def inferenceAlgorithm(gm ,alg, accumulator=None,parameter=None,dtype=numpy.float32):
+   """Factory function to construct an inference object.
+
+   Keyword arguments:
+   
+   gm -- a graphical model
+   
+   alg -- the inference algorithm ('bp','trbp','icm','astar','lf','a-expansion','ab-swap','graphcut',Lazyflipper',...)
+   
+   accumulator -- accumulator ('minimizer','maximizer') (default: depends on gm operator)
+      If the operator of the gm is ``\'adder\'`` the default
+      accumulator is ``\'minimizer\'`` .
+      If the operator of the gm is ``\'multiplier\'`` the default
+      accumulator is ``\'maximizer\'`` .
+   parameter -- parameter object of the inference object
+      Construct a parameter object with ``inferenceParameter(gm ,alg, accumulator=None)``
+   Construct an inference object::
+   
+      #TODO
+      
+   """
    alg=alg.lower()
    #evaluate accumulator
    operator=gm.operator
@@ -114,25 +150,23 @@ def inferenceAlgorithm(gm ,alg, accumulator=None,parameter=None):
       elif operator=='multiplier':
          accumulator='maximizer'
       else :
-         print operator
+         print "selected operator : ", operator
          raise NameError('operator must be \'adder\' or \'multiplier\'') 
    else:
       accumulator=accumulator.lower()
-      if accumulator is 'min' or 'minimizer':
+      if accumulator == 'min' or accumulator == 'minimizer':
          accumulator='minimizer'
-      elif accumulator is 'max' or 'maximizer':
+      elif accumulator == 'max' or accumulator == 'maximizer':
          accumulator='maximizer'
       else :
-         print operator
+         print "selected accumulator: ", accumulator
          raise NameError('operator must be \'minimizer\' or \'maximizer\' (or just \'min\' or \'max\' )')   
    #evaluate inference algorithm      
    name = 'inference.'+operator+'.'+accumulator
    #dirty hack!
    if alg=='bp' or alg=='beliefpropagation':
-      if parameter == None: 
-         return eval(name).Bp(gm)
-      else: 
-         return eval(name).Bp(gm,parameter)
+      if parameter == None: return eval(name).Bp(gm)
+      else: return eval(name).Bp(gm,parameter)
    if alg=='trwbp' or alg=='trbp':
       if parameter is None: return eval(name).TrBp(gm)
       else: return eval(name).TrBp(gm,parameter)
@@ -179,13 +213,61 @@ def inferenceAlgorithm(gm ,alg, accumulator=None,parameter=None):
    else:
       raise NameError( 'alg \'' + alg + '\' is unknown') 
       
+
       
-def pottsFunction(shape,diag,nondiag):
+def pottsFunction(shape,diag=0.0,nondiag=1.0,dtype=numpy.float32):
+   """Factory function to construct a numpy array which encodes a potts-function.
+
+   Keyword arguments:
+   
+   shape -- shape / number of of labels of the potts-function 
+   
+   diag  -- value if labels are equal (defualt :) 
+
+   nondiag -- value if labels are not equal
+
+   dtype -- data type of the numpy array
+   
+   get a potts-function ::
+   
+      f=opengm.pottsFunction([2,2],diag=0.0,nondiag=1.0)
+      
+   """
    f=numpy.ones(shape,dtype=numpy.float32)
    f.fill(nondiag)
    numpy.fill_diagonal(f, diag)
    return f
+
+def differenceFunction(shape,norm=2,weight=1.0,truncate=None,dtype=numpy.float32):
+   """Factory function to construct a numpy array which encodes a difference-function.
+   The difference can be of any norm (1,2,...) and can be truncated or untruncated.
+
+   Keyword arguments:
    
+   shape -- shape / number of of labels of the potts-function 
+   
+   weight  -- weight which is multiplied to the norm
+
+   truncate -- truncate all values where the norm is bigger than truncate
+
+   dtype -- data type of the numpy array
+   
+   get a truncated squared difference function ::
+   
+      f=opengm.differenceFunction([2,4],weight=0.5,truncate=5)
       
-
-
+   """
+   assert len(shape)==2
+   f=numpy.ones(shape,dtype=numpy.float32)
+   if shape[0]<shape[1]:
+      yVal=numpy.arange(0,shape[1])
+      for x in range(shape[0]):
+         f[x,:]=(numpy.abs(x-yVal)**norm)
+   else :
+      xVal=numpy.arange(0,shape[0])
+      for y in range(shape[1]):
+         f[:,y]=(numpy.abs(xVal-y)**norm)
+   if truncate!=None:
+      f[numpy.where(f>truncate)]=truncate      
+   f*=weight
+   return f
