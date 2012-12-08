534e2d68176a315ca1ac7b8e7cf1d6e62535aea4
tbeier <tbeier@tbeier-machine.(none)>
2012-12-08 13:10:11 +0100

    improved cplex wrapper -adding constraint is possible (via lists so far) -adding constraintS is possible via lists of lists

--- src/interfaces/python/opengm/inference/pyCplex.cxx
+++ src/interfaces/python/opengm/inference/pyCplex.cxx
@@ -37,6 +37,89 @@ namespace pycplex{
       p.timeLimit_=timeLimit;
    }
 
+
+   template<class INF,class VALUE_TYPE,class INDEX_TYPE>
+   void addConstraintPythonList
+   (
+      INF & inf,
+      const boost::python::list & lpVariableIndices,
+      const boost::python::list & coefficients,
+      const VALUE_TYPE lowerBound,
+      const VALUE_TYPE upperBound
+   ) {
+      typedef PythonFundamentalListAccessor<VALUE_TYPE,true> FloatAccessor;
+      typedef opengm::AccessorIterator<FloatAccessor,true> FloatIterator;
+      typedef PythonFundamentalListAccessor<INDEX_TYPE,true> IntAccessor;
+      typedef opengm::AccessorIterator<IntAccessor,true> IntIterator;
+      // lp-variables iterator begin and end
+      IntAccessor lpVarAccessor(coefficients);
+      IntIterator lpVarBegin(lpVarAccessor,0);
+      IntIterator lpVarEnd(lpVarAccessor,lpVarAccessor.size());
+      // coefficient iterator end
+      FloatAccessor coeffAccessor(coefficients);
+      FloatIterator coeffBegin(coeffAccessor,0);
+      // add constraint
+      inf.addConstraint(lpVarBegin,lpVarEnd,coeffBegin,lowerBound,upperBound);
+   }
+
+   template<class INF,class VALUE_TYPE,class INDEX_TYPE>
+   void addConstraintsPythonLists
+   (
+      INF & inf,
+      const boost::python::list & lpVariableIndices,
+      const boost::python::list & coefficients,
+      const boost::python::list lowerBounds,
+      const boost::python::list upperBounds
+   ) {
+      typedef PythonFundamentalListAccessor<VALUE_TYPE,true> FloatAccessor;
+      typedef opengm::AccessorIterator<FloatAccessor,true> FloatIterator;
+      //
+      const size_t nVi     = static_cast<size_t>(boost::python::len(lpVariableIndices));
+      const size_t nCoeff  = static_cast<size_t>(boost::python::len(coefficients));
+      const size_t nLBound = static_cast<size_t>(boost::python::len(lowerBounds));
+      const size_t nUBound = static_cast<size_t>(boost::python::len(upperBounds));
+      FloatAccessor lBAccessor(lowerBounds);
+      FloatAccessor uBAccessor(upperBounds);
+      FloatIterator lBBegin(lBAccessor,0);
+      FloatIterator uBBegin(uBAccessor,0);
+      if(nVi==nCoeff || nCoeff==1){
+         // all the same length
+         if( (nLBound==nUBound && nLBound == nVi)  || nLBound==1 || nUBound==1){
+            // accessors for lower and upper bounds
+            // loop over all constraints 
+            // which should be added
+            for(size_t c=0;c<nVi;++c){
+               boost::python::list lpVar;
+               boost::python::list coeff;
+               // extract lpvar list
+               {
+               boost::python::extract<boost::python::list> extractor(lpVariableIndices[c]);
+               if(extractor.check())
+                  lpVar=extractor();
+               else
+                  throw opengm::RuntimeError("lpVariableIndices must be a list of lists");
+               }
+               {
+               // extract coefficients list
+               boost::python::extract<boost::python::list> extractor(coefficients[nCoeff==1 ? 0:c ]);
+               if(extractor.check())
+                  lpVar=extractor();
+               else
+                  throw opengm::RuntimeError("coefficients must be a list of lists");
+               }
+               // call funtion which adds a single constraint from a lists
+               pycplex::addConstraintPythonList<INF,VALUE_TYPE,INDEX_TYPE>(inf,lpVar,coeff,lBBegin[ nLBound==1 ? 0:c ],uBBegin[nUBound==1 ? 0:c]);
+            }
+         }
+         else{
+            throw opengm::RuntimeError("len(lowerBounds) and len(upperBounds) must be equal to 1 or len(coefficients)");
+         }
+      }
+      else{
+         throw opengm::RuntimeError("len(coefficients) must be 1 or equal to len(lpVariableIndices)");
+      }
+   }
+      
 }
 
 
@@ -47,6 +130,8 @@ void export_cplex() {
    // import numpy c-api
    import_array();
    // Inference typedefs
+   typedef typename GM::ValueType ValueType;
+   typedef typename GM::IndexType IndexType;
    typedef opengm::LPCplex<GM, ACC> PyLPCplex;
    typedef typename PyLPCplex::Parameter PyLPCplexParameter;
    typedef typename PyLPCplex::VerboseVisitorType PyLPCplexVerboseVisitor;
@@ -78,7 +163,10 @@ void export_cplex() {
    // export inference via macro
    OPENGM_PYTHON_INFERENCE_NO_RESET_EXPORTER(PyLPCplex, "LPCplex",   
    "TODO:\n\n"
-   );
+   )
+   .def("addConstraint", &pycplex::addConstraintPythonList<PyLPCplex,ValueType,IndexType>  )
+   .def("addConstraints", &pycplex::addConstraintsPythonLists<PyLPCplex,ValueType,IndexType>  )
+   ;
 }
 // explicit template instantiation for the supported semi-rings
 template void export_cplex<GmAdder, opengm::Minimizer>();
