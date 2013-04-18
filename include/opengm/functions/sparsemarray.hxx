c46cc78f883ec8adf60aa1e57bb2b73004590894
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-04-18 19:28:38 +0200

    minor refactoring

--- include/opengm/functions/sparsemarray.hxx
+++ include/opengm/functions/sparsemarray.hxx
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef OPENGM_SPARSEMARRAY_FUNCTION_HXX
-#define OPENGM_SPARSEMARRAY_FUNCTION_HXX
+#ifndef OPENGM_SPARSE_FUNCTION
+#define OPENGM_SPARSE_FUNCTION
 
 #include "opengm/datastructures/sparsemarray/sparsemarray.hxx"
 #include "opengm/functions/function_registration.hxx"
@@ -10,126 +10,111 @@ namespace opengm {
 
 /// \cond HIDDEN_SYMBOLS
 /// FunctionRegistration
-template<class T>
-struct FunctionRegistration<opengm::SparseMarray<T> > {
+template<class T,class I,class L,class C>
+struct FunctionRegistration<opengm::SparseFunction<T,I,L,C> > {
    enum ID { Id = opengm::FUNCTION_TYPE_ID_OFFSET + 1 };
 };
 
 /// FunctionSerialization
 ///
 /// \ingroup functions
-template<class T>
-class FunctionSerialization< opengm::SparseMarray<T> > {
+template<class T,class I,class L,class C>
+class FunctionSerialization< opengm::SparseFunction<T,I,L,C> > {
 public:
-   typedef typename opengm::SparseMarray<T>::ValueType ValueType;
+   typedef typename opengm::SparseFunction<T,I,L,C>::ValueType ValueType;
 
-   static size_t indexSequenceSize(const opengm::SparseMarray<T>&);
-   static size_t valueSequenceSize(const opengm::SparseMarray<T>&);
+   static size_t indexSequenceSize(const opengm::SparseFunction<T,I,L,C>&);
+   static size_t valueSequenceSize(const opengm::SparseFunction<T,I,L,C>&);
    template<class INDEX_OUTPUT_ITERATOR,class VALUE_OUTPUT_ITERATOR >
-      static void serialize(const opengm::SparseMarray<T>&, INDEX_OUTPUT_ITERATOR,VALUE_OUTPUT_ITERATOR );
+      static void serialize(const opengm::SparseFunction<T,I,L,C>&, INDEX_OUTPUT_ITERATOR,VALUE_OUTPUT_ITERATOR );
    template<class INDEX_INPUT_ITERATOR ,class VALUE_INPUT_ITERATOR>
-      static void deserialize( INDEX_INPUT_ITERATOR,VALUE_INPUT_ITERATOR, opengm::SparseMarray<T>&);
+      static void deserialize( INDEX_INPUT_ITERATOR,VALUE_INPUT_ITERATOR, opengm::SparseFunction<T,I,L,C>&);
 };
 /// \endcond
 
-template<class T>
+template<class T,class I,class L,class C>
 inline size_t
-FunctionSerialization<opengm::SparseMarray<T> >::indexSequenceSize
+FunctionSerialization<opengm::SparseFunction<T,I,L,C> >::indexSequenceSize
 (
-   const opengm::SparseMarray<T>& src
+   const opengm::SparseFunction<T,I,L,C>& src
 ) {
-   if(src.dimension() == 0) {
-      return 1;
-   }
-   else {
-      return 1 + src.dimension()+1+src.getAssociativeContainer().size();
-   }
+   return 1 + src.dimension()+1+src.container().size();
 }
 
-template<class T>
+template<class T,class I,class L,class C>
 inline size_t
-FunctionSerialization<opengm::SparseMarray<T> >::valueSequenceSize
+FunctionSerialization<opengm::SparseFunction<T,I,L,C> >::valueSequenceSize
 (
-   const opengm::SparseMarray<T>& src
+   const opengm::SparseFunction<T,I,L,C>& src
 ) {
-   if(src.dimension()==0) {
-      return 1;
-   }
-   else{
-      return src.getAssociativeContainer().size()+1;
-   }
+
+   return src.container().size()+1;
 }
 
-template<class T>
+template<class T,class I,class L,class C>
 template<class INDEX_OUTPUT_ITERATOR,class VALUE_OUTPUT_ITERATOR >
 void
-FunctionSerialization<opengm::SparseMarray<T> >::serialize
+FunctionSerialization<opengm::SparseFunction<T,I,L,C> >::serialize
 (
-   const opengm::SparseMarray<T>& src,
+   const opengm::SparseFunction<T,I,L,C>& src,
    INDEX_OUTPUT_ITERATOR indexOutIterator,
    VALUE_OUTPUT_ITERATOR valueOutIterator
 ) {
-   if(src.dimension()==0) {
-      *indexOutIterator=0;
-      *valueOutIterator=src(0);
+   *indexOutIterator=src.dimension();
+   ++indexOutIterator;
+   for(size_t i=0;i<src.dimension();++i) {
+      *indexOutIterator=src.shape(i);
+      ++indexOutIterator;
    }
-   else{
-      for(size_t i=0;i<src.dimension();++i) {
-         *indexOutIterator=src.shape(i);
-         ++indexOutIterator;
-      }
-      //save the default value
-      *valueOutIterator=src.getDefaultValue();
+   //save the default value
+   *valueOutIterator=src.defaultValue();
+   ++valueOutIterator;
+   //save how many not default values are in the SparseFunction
+   *indexOutIterator=src.container().size();
+   ++indexOutIterator;
+   typedef typename opengm::SparseFunction<T,I,L,C>::ContainerType::const_iterator IterType;
+   IterType srcIter=src.container().begin();
+   for(size_t i=0;i<src.container().size();++i) {
+      *indexOutIterator=srcIter->first;
+      *valueOutIterator=srcIter->second;
       ++valueOutIterator;
-      //save how many not default values are in the sparsemarray
-       *indexOutIterator=src.getAssociativeContainer().size();
-      //map begin
-      typedef typename opengm::SparseMarray<T>::const_assigned_assoziative_iterator IterType;
-      IterType srcIter=src.assigned_assoziative_begin();
-
-      for(size_t i=0;i<src.getAssociativeContainer().size();++i) {
-         *indexOutIterator=srcIter->first;
-         *valueOutIterator=srcIter->second;
-         ++valueOutIterator;
-         ++indexOutIterator;
-         ++srcIter;
-      }
+      ++indexOutIterator;
+      ++srcIter;
    }
 }
 
-template<class T>
+template<class T,class I,class L,class C>
 template<class INDEX_INPUT_ITERATOR,class VALUE_INPUT_ITERATOR >
 void
-FunctionSerialization<opengm::SparseMarray<T> >::deserialize
+FunctionSerialization<opengm::SparseFunction<T,I,L,C> >::deserialize
 (
    INDEX_INPUT_ITERATOR indexOutIterator,
    VALUE_INPUT_ITERATOR valueOutIterator,
-   opengm::SparseMarray<T>& dst
+   opengm::SparseFunction<T,I,L,C>& dst
 ) {
-   if(*indexOutIterator==0) {
-      size_t shape [] ={0};
-      //empty shape to get a scalar array
-      dst.init(shape, shape,*valueOutIterator);
-   }
-   else{
-      const size_t dim=*indexOutIterator;
-      std::vector<size_t> shape(dim);
+
+   const size_t dim=*indexOutIterator;
+   std::vector<size_t> shape(dim);
+   ++indexOutIterator;
+   for(size_t i=0;i<dim;++i) {
+      shape[i]=*indexOutIterator;
       ++indexOutIterator;
-      for(size_t i=0;i<dim;++i) {
-         shape[i]=*indexOutIterator;
-         ++indexOutIterator;
-      }
-      dst.init(shape.begin(),shape.end(),*valueOutIterator);
-      const size_t nNonDefault=*indexOutIterator;
-      for(size_t i=0;i<nNonDefault;++i) {
-         dst.reference(*indexOutIterator)=*valueOutIterator;
-         ++valueOutIterator;
-         ++indexOutIterator;
-      }
    }
+   dst = opengm::SparseFunction<T,I,L,C>(shape.begin(),shape.end(),*valueOutIterator);
+   ++valueOutIterator;
+   const size_t nNonDefault=*indexOutIterator;
+   ++indexOutIterator;
+   typedef typename opengm::SparseFunction<T,I,L,C>::KeyValPairType KeyValPairType;
+   for(size_t i=0;i<nNonDefault;++i) {
+
+      dst.container().insert(KeyValPairType(*indexOutIterator,*valueOutIterator));
+      ++valueOutIterator;
+      ++indexOutIterator;
+   }
+   
 }
 
 } // namepsace opengm
 
-#endif // OPENGM_SPARSEMARRAY_FUNCTION_HXX
+#endif // OPENGM_SPARSE_FUNCTION
 
