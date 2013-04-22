2dbb2b60c57c3ba99035e976579850bf658c89cb
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-04-22 12:47:26 +0200

    HOTFIX for 32 bit problem: - there where some hardcoded size_t's   replaced one with SubGm::LabelType - added a template to avoid errors

--- include/opengm/inference/dualdecomposition/dualdecomposition_base.hxx
+++ include/opengm/inference/dualdecomposition/dualdecomposition_base.hxx
@@ -181,7 +181,7 @@ namespace opengm {
    protected:
       template<class ITERATOR> void addDualBlock(const SubFactorListType&,ITERATOR,ITERATOR);
       std::vector<DualVariableType*> getDualPointers(size_t);
-      template<class ACC> void getBounds(const std::vector<std::vector<LabelType> >&, const std::vector<SubVariableListType>&, ValueType&, ValueType&, std::vector<LabelType>&);
+      template<class ACC> void getBounds(const std::vector<std::vector<typename SubGmType::LabelType> >&, const std::vector<SubVariableListType>&, ValueType&, ValueType&, std::vector<LabelType>&);
       double subGradientNorm(double L=1) const;
       virtual DualDecompositionBaseParameter& parameter() = 0;
       virtual void allocate() = 0;
@@ -366,7 +366,7 @@ namespace opengm {
    template <class ACC>
    void DualDecompositionBase<GM,DUALBLOCK>::getBounds
    (
-      const std::vector<std::vector<LabelType> >& subStates,
+      const std::vector<std::vector<typename SubGmType::LabelType> >& subStates,
       const std::vector<SubVariableListType>& subVariableLists,
       ValueType& lowerBound, 
       ValueType& upperBound,
