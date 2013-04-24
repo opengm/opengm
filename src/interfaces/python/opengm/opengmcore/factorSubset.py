16a95ad94b3e65ff2474f552afb5dcee763c68a0
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-04-24 17:17:33 +0200

    renamed files in python wrapper

---
+++ src/interfaces/python/opengm/opengmcore/factorSubset.py
@@ -0,0 +1,84 @@
+import numpy 
+from dtypes import index_type,value_type,label_type 
+
+class FactorSubset(object):
+    """ Holds a subset of factor indices of a graphical model.
+        This class is used to compute queries for a a subset of a gm.
+        This queries are very efficient since allmost all members
+        are implemented in pure C++.
+        The members are a vectorized subset of the regular 
+        factor api of a graphical model. Therefore allmost all factor queries 
+        can be vectorized with this class.
+
+    Args :
+
+        gm : the graphical model to which the factors belong
+
+        factorIndices : the factor indices w.r.t. the gm which are in the subset . 
+            If factorIndices is not given, the indices of all factors will be used
+
+    Example: ::
+
+        >>> import opengm
+        >>> import numpy
+        >>> unaries=numpy.random.rand(100, 100,2).astype(numpy.float32)
+        >>> gm=opengm.grid2d2Order(unaries,opengm.PottsFunction([2,2],0.0,0.4))
+        >>> factorSubset=opengm.FactorSubset(gm)
+        >>> len(factorSubset)==gm.numberOfFactors
+        True
+        >>> numberOfVariables=factorSubset.numberOfVariables()
+        >>> len(numberOfVariables)==gm.numberOfFactors
+        True
+
+    """
+    def __init__(self,gm,factorIndices=None):
+        self.gm=gm
+        if factorIndices is None:
+            self.factorIndices=numpy.arange(gm.numberOfFactors,dtype=index_type)
+        else :
+            self.factorIndices=factorIndices
+
+    def __len__(self):
+        return len(self.factorIndices)
+
+
+    def numberOfVariables(self):
+        return self.gm._factor_numberOfVariables(self.factorIndices)
+
+    def evaluateLabeling(self,labels):
+        if(labels.ndim==1):
+            return self.gm._factor_evaluateGmLabeling(self.factorIndices,labels)
+        elif(labels.ndim==2):
+            return self.gm._factor_gmLablingToFactorLabeling(self.factorIndices,labels)
+
+    def variableIndices(self):
+        return self.gm._factor_variableIndices(self.factorIndices)
+
+    def numberOfLabels(self):
+        return self.gm._factor_numberOfLabels(self.factorIndices)
+
+    def isSubmodular(self):
+        return self.gm._factor_isSubmodular(self.factorIndices)
+
+    def mapScalarReturning(self,function,dtype):
+        if(dtype==numpy.float32):
+            return self.gm._factor_scalarRetFunction_float32(function,self.factorIndices)
+        elif(dtype==numpy.float64):
+            return self.gm._factor_scalarRetFunction_float64(function,self.factorIndices)
+        elif(dtype==numpy.uint64):
+            return self.gm._factor_scalarRetFunction_uint64(function,self.factorIndices)
+        elif(dtype==numpy.int64):
+            return self.gm._factor_scalarRetFunction_int64(function,self.factorIndices)
+        elif(dtype==numpy.bool):
+            return self.gm._factor_scalarRetFunction_bool(function,self.factorIndices)
+        else:
+            raise RuntimeError("dtype %s is not supported, so far only float32, float64, int64, uint64 and bool are supported")% (str(dtype),)
+
+    def withOrder(self,order):
+        return self.gm._factor_withOrder(factorIndices,int(order))
+
+
+if __name__ == "__main__":
+  import doctest
+  import opengm
+  doctest.testmod()
