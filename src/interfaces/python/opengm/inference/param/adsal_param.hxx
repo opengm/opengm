1e9a34aa43624d72e38d3be2ca011a1680223137
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-05-31 00:27:50 +0200

    -a

---
+++ src/interfaces/python/opengm/inference/param/adsal_param.hxx
@@ -0,0 +1,45 @@
+#ifndef ADSAI_EXTERNAL_PARAM
+#define ADSAI_EXTERNAL_PARAM
+
+#include "param_exporter_base.hxx"
+//solver specific
+#include <opengm/inference/trws/trws_adsal.hxx>
+
+using namespace boost::python;
+
+template<class INFERENCE>
+class InfParamExporterAdsai{
+
+public:
+   typedef typename INFERENCE::ValueType ValueType;
+   typedef typename INFERENCE::Parameter Parameter;
+   typedef InfParamExporterAdsai<INFERENCE> SelfType;
+
+
+   static void set 
+   (
+      Parameter & p,
+      const size_t numberOfIterations
+   ) {
+      p.maxNumberOfIterations()=numberOfIterations;
+   }
+
+   void static exportInfParam(const std::string & className){
+      class_<Parameter > ( className.c_str(),init<>())
+         .def ("set", &SelfType::set, 
+            (
+               arg("steps")=1000
+            )
+         )
+         .def_readwrite("steps",&Parameter::maxNumberOfIterations, "number of iterations")
+      ;
+   }
+};
+
+template<class GM,class ACC>
+class InfParamExporter<opengm::ADSal<GM,ACC> >  
+: public  InfParamExporterAdsai<opengm::ADSal<GM,ACC> > {
+
+};
+
+#endif
