53ce75f50067c6e5362baef4fac0faf389243d2c
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-10-09 09:44:19 +0200

    wip

--- include/opengm/inference/pbp.hxx
+++ include/opengm/inference/pbp.hxx
@@ -1,6 +1,6 @@
 #pragma once
-#ifndef OPENGM_ICM_HXX
-#define OPENGM_ICM_HXX
+#ifndef OPENGM_PBP_HXX
+#define OPENGM_PBP_HXX
 
 #include <vector>
 #include <string>
@@ -9,12 +9,63 @@
 #include "opengm/opengm.hxx"
 #include "opengm/inference/visitors/visitor.hxx"
 #include "opengm/inference/inference.hxx"
-
+#include "opengm/datastructures/dynamic_priority_queue.hxx"
 
 namespace opengm {
-  
+
+   namespace detail_pbp {
+      template<class GM>
+      class Priority{
+      public:
+         Priority(const GM & gm)
+         :   pQueue_(gm.numberOfVariables())
+         {
+            for(UInt64Type vi=0;vi<gm.numberOfVariables();++vi){
+               pQueue_.insert(vi,gm.numberOfLabels(vi));
+            }
+         }   
+
+         UInt64Type highestPriorityVi()const{
+            OPENGM_CHECK(!pQueue_.isEmpty(),"");
+            return pQueue_.minIndex();
+         }
+
+         UInt64Type getAndRemoveHighestPriorityVi(){
+            OPENGM_CHECK(!pQueue_.isEmpty(),"");
+            return pQueue_.deleteMin();
+         }
 
 
+         void removeVi(const UInt64Type vi){
+            OPENGM_CHECK(!pQueue_.isEmpty(),"");
+            OPENGM_CHECK(pQueue_.contains(vi),"");
+            pQueue_.deleteKey(vi);
+         }
+
+         void addVi(const UInt64Type vi,const UInt64Type newNLabels){
+            //OPENGM_CHECK(!pQueue_.isEmpty(),"");
+            OPENGM_CHECK(!pQueue_.contains(vi),"");
+            pQueue_.insert(vi,newNLabels);
+         }
+
+
+
+         void changeConfusingSetSize(const UInt64Type vi,const UInt64Type oldNlabels,const UInt64Type newNLabels){
+            OPENGM_CHECK(!pQueue_.isEmpty(),"");
+            OPENGM_CHECK(pQueue_.contains(vi),"");
+            OPENGM_CHECK_OP(oldNlabels,!=,newNLabels,"");
+            if(newNLabels<oldNlabels)
+               pQueue_.decreaseKey(vi,newNLabels);
+            else
+               pQueue_.increaseKey(vi,newNLabels);
+         }
+
+      private:
+
+         opengm::MinIndexedPQ pQueue_;
+      };
+   }
+/*
 
 template<class GM>
 class ActiveLabels{
@@ -51,9 +102,6 @@ private:
 };
 
 
-
-
-
 template<class GM>
 class SparseMessage{
 public:
@@ -86,10 +134,6 @@ private:
    bool isInit_;
    ValueType uninitValue_;
 };
-
-
-
-
 
 
 template<class FACTOR,class VAR_TO_FAC_MSG_CONT,class FAC_TO_VAR_MSG>
@@ -150,32 +194,167 @@ void fac_to_var_msg(
 
 
 
+template<class GM>
+class ActiveLabelSpace{
+public:
+   typedef GM GraphicalModelType;
+   typedef typename GraphicalModelType::LabelType LabelType;
+   typedef typename GraphicalModelType::IndexType IndexType;
+
+   LabelType numberOfActiveLabels(const IndexType vi )const{
+      return space_[vi].size();
+   }
+
+   void removeLabel(const IndexType vi,const LabelType label)const{
+      OPENGM_CHECK(space_[vi].find(label)!=space_[vi].end(),"label has already been removed");
+   }
+
+   LabelType getLabelAtIndex(const IndexType vi,const IndexType labelIndex){
+      OPENGM_CHECK_OP(labelIndex,<,space_[vi].size(),"labelIndex is bigger then allowed");
+   }
+
+private:   
+   std::vector< RandomAccessSet<LabelType>  > space_;
+};
+
+*/
+template<class GM>
+class MsgBase{
+public:
+   typedef typename GM::LabelType LabelType;
+   typedef typename GM::IndexType IndexType;
+   typedef typename GM::ValueType ValueType;
+
+   typedef std::map<LabelType,ValueType> MapType;
+
+   MsgBase(const UInt64Type from=0, const UInt64Type to=0,const UInt64Type numberOfLabels=0)
+   :  from_(from),
+      to_(to),
+      numberOfLabels_(numberOfLabels),
+      numberOfActiveLabels_(numberOfLabels)
+   {
+      assertions();
+   }
+
+   void setFromTo(const UInt64Type from, const UInt64Type to){
+      assertions();
+      from_=from;
+      to_=to;
+      assertions();
+   }
+   UInt64Type from()const{
+      return from_;
+   }
+   UInt64Type to()const{
+      return to_;
+   }
+
+   void setNumberOfLabels(const UInt64Type numberOfLabels){
+      assertions();
+      numberOfLabels_=numberOfLabels;
+      numberOfActiveLabels_=numberOfLabels;
+      assertions();
+   }
+
+   bool hasInformation()const{
+      assertions();
+      return valueMap_.empty()==false;
+   }
+
+
+   void assertions()const{
+      OPENGM_CHECK_OP(numberOfActiveLabels_,<=,numberOfLabels_,"");
+      OPENGM_CHECK(numberOfActiveLabels_>=2 || (numberOfLabels_==0) ,"");
+      OPENGM_CHECK(numberOfActiveLabels_>=2 || (numberOfLabels_==0 && numberOfActiveLabels_==0) ,"");
+      OPENGM_CHECK( valueMap_.size()<=0 || valueMap_.size()==numberOfActiveLabels_,"");
+      OPENGM_CHECK( valueMap_.size()!=numberOfLabels_ || valueMap_.size()==0," ");
+   }
+
+   template<class LABEL_ITER>
+   void pruneLabels
+   (
+      LABEL_ITER begin,
+      LABEL_ITER end
+   ){
+      this->assertions();
+      if(valueMap_.size()==0){
+
+      }
+      else{
+         // ASSERTIONS
+         const size_t nErase=std::distance(begin,end);
+         for(size_t i=0;i<nErase;++i){
+            OPENGM_CHECK(valueMap_.find(begin[i])!=valueMap_.end(),"some label has been erased");
+         }
+         while(begin!=end)
+            valueMap_.erase(*begin);
+      }
+      this->assertions();
+   }
+
+   size_t mapSize()const{
+      return valueMap_;
+   }
+
+private:
+
+   UInt64Type from_;
+   UInt64Type to_;
+   UInt64Type numberOfLabels_;
+   UInt64Type numberOfActiveLabels_;
+
+   MapType valueMap_;
+};
+
+
+
+template<class GM>
+class VarToFacMsg : public MsgBase<GM> {
+public:
+   VarToFacMsg(const UInt64Type from=0, const UInt64Type to=0)
+   :  MsgBase<GM>(from,to) {
+
+   }
+
+};
+
+template<class GM>
+class FacToVarMsg : public MsgBase<GM> {
+public:
+   FacToVarMsg(const UInt64Type from=0, const UInt64Type to=0)
+   :  MsgBase<GM>(from,to) {
+
+   }
+};
 
 
 
 template<class GM, class ACC>
-class ICM : public Inference<GM, ACC>
+class PBP : public Inference<GM, ACC>
 {
 public:
 
    typedef ACC AccumulationType;
    typedef GM GraphicalModelType;
    OPENGM_GM_TYPE_TYPEDEFS;
-   typedef Movemaker<GraphicalModelType> MovemakerType;
-   typedef VerboseVisitor<ICM<GM,ACC> > VerboseVisitorType;
-   typedef EmptyVisitor<ICM<GM,ACC> > EmptyVisitorType;
-   typedef TimingVisitor<ICM<GM,ACC> > TimingVisitorType;
+   typedef VerboseVisitor<PBP<GM,ACC> > VerboseVisitorType;
+   typedef EmptyVisitor<PBP<GM,ACC> > EmptyVisitorType;
+   typedef TimingVisitor<PBP<GM,ACC> > TimingVisitorType;
+
+   typedef FacToVarMsg<GM> FacToVarMsgType;
+   typedef VarToFacMsg<GM> VarToFacMsgType;
 
    class Parameter {
    public:
       Parameter(
+         const size_t steps=10
       )
-      : {
-
+      :  steps_(steps){
       }
-
+      size_t steps_;
    };
-   ICM(const GraphicalModelType&, const Parameter& = Parameter());
+
+   PBP(const GraphicalModelType&, const Parameter& = Parameter());
    std::string name() const;
    const GraphicalModelType& graphicalModel() const;
    InferenceTermination infer();
@@ -193,84 +372,353 @@ public:
    }
 
 private:
-      const GraphicalModelType& gm_;
-      Parameter param_;
+
+   void forwardPass();
+   void backwardPass();
+   void getInitialPriorities();
+
+   void updateBeliefAndPriority(const IndexType vi);
+
+   void applyLabelPruning(const IndexType  vi);
+   void computeFacToVarMsg(const IndexType fi,const IndexType vi);
+   void computeVarToFacMsg(const IndexType vi,const IndexType fi);
+
+
+
+   const GraphicalModelType& gm_;
+   std::vector<RandomAccessSet<IndexType> > viAdj_;
+   Parameter param_;
+
+   // message indexing
+   std::vector<UInt64Type> facToVarStar_;
+   std::vector<UInt64Type> varToFacStart_;
+   std::vector<std::map<UInt64Type,UInt64Type> > facToVarMap_;
+   std::vector<std::map<UInt64Type,UInt64Type> > varToFacMap_;
+
+
+   // message containers
+   std::vector<FacToVarMsg<GM> > facToVarMsg_;
+   std::vector<VarToFacMsg<GM> > varToFacMsg_;
+
+   // timestamps 
+   std::vector<IndexType >   forwardTime_;
+   std::vector<LabelType >   confusigSetSize_;
+
+   // 
+   detail_pbp::Priority<GM>  queue_;
+   std::vector<bool>         isCommited_;
+
+   std::vector < std::set<LabelType > > activeLabels_;
 };
 
 
 
 template<class GM, class ACC>
-ICM<GM, ACC>::ICM
+PBP<GM, ACC>::PBP
 (
       const GraphicalModelType& gm,
       const Parameter& parameter
 )
 :  gm_(gm),
-   param_(parameter)
+   param_(parameter),
+   facToVarStar_(gm.numberOfFactors()),
+   varToFacStart_(gm.numberOfVariables()),
+   facToVarMap_(gm.numberOfFactors()),
+   varToFacMap_(gm.numberOfVariables()),
+   facToVarMsg_(),
+   varToFacMsg_(),
+   forwardTime_(gm.numberOfVariables()),
+   confusigSetSize_(gm.numberOfVariables()),
+   queue_(gm),
+   isCommited_(gm.numberOfVariables(),false),
+   activeLabels_(gm.numberOfVariables())
 {
+   std::cout<<"0\n";
+   gm_.variableAdjacencyList(viAdj_);
+   // counting the messages
+   Int64Type varToFacCounter=0;
+   UInt64Type facToVarCounter=0;
+
+   for(IndexType vi=0;vi<gm_.numberOfVariables();++vi){
+      OPENGM_CHECK_OP(gm_.numberOfLabels(vi),>=,2,"wrong gm");
+      const IndexType nFac=gm_.numberOfFactors(vi);
+      varToFacStart_[vi]=varToFacCounter;
+      varToFacCounter+=nFac;
+
+      confusigSetSize_[vi]=gm_.numberOfLabels(vi);
+
+      for(LabelType l=0;l<gm_.numberOfLabels(vi);++l){
+         activeLabels_[vi].insert(l);
+      }
+   }
+   std::cout<<"1\n";
+   for(IndexType fi=0;fi<gm_.numberOfFactors();++fi){
+      const IndexType nVar=gm_[fi].numberOfVariables();
+      facToVarStar_[fi]=facToVarCounter;
+      facToVarCounter+=nVar;
+   }
+   std::cout<<"2\n";
+   // resizing message containers
+   varToFacMsg_.resize(varToFacCounter);
+   facToVarMsg_.resize(facToVarCounter);
+   facToVarCounter=0;
+   varToFacCounter=0;
+   std::cout<<"3\n";
+   // fill message informations
+   for(IndexType vi=0;vi<gm_.numberOfVariables();++vi){
+      const IndexType nFac=gm_.numberOfFactors(vi);
+      for(IndexType f=0;f<nFac;++f){
+         varToFacMsg_[varToFacCounter].setFromTo(vi,gm_.factorOfVariable(vi,f));
+         varToFacMsg_[varToFacCounter].setNumberOfLabels(gm_.numberOfLabels(vi));
+         varToFacMap_[vi][gm_.factorOfVariable(vi,f)]=varToFacCounter;
+         ++varToFacCounter;
+      }
+   }
+   std::cout<<"4\n";
+   for(IndexType fi=0;fi<gm_.numberOfFactors();++fi){
+      const IndexType nVar=gm_[fi].numberOfVariables();
+      for(IndexType v=0;v<nVar;++v){
+         facToVarMsg_[facToVarCounter].setFromTo(fi,gm_.variableOfFactor(fi,v));
+         facToVarMsg_[facToVarCounter].setNumberOfLabels(gm_.numberOfLabels(gm_.variableOfFactor(fi,v)));
+         facToVarMap_[fi][gm_.variableOfFactor(fi,v)]=facToVarCounter;
+         ++facToVarCounter;
+      }
+   }
+   std::cout<<"5\n";
+}
+
+
+template<class GM, class ACC>
+void PBP<GM, ACC>::getInitialPriorities(){
 
 }
-      
+
+template<class GM, class ACC>
+void PBP<GM, ACC>::updateBeliefAndPriority(const typename PBP<GM, ACC>::IndexType vi){
+
+}
+
+
+template<class GM, class ACC>
+void PBP<GM, ACC>::applyLabelPruning(const typename PBP<GM, ACC>::IndexType  vi){
+
+   std::vector<LabelType> labelsToPrune;
+   // .... 
+   // ....
+   // ....
+   if(!labelsToPrune.empty()){
+      // prune all variable to factor msg
+      const IndexType nFac = gm_.numberOfFactors(vi);
+      for(IndexType f=0;f<nFac;++f){
+         const IndexType fi=gm_.factorOfVariable(vi,f);
+
+         varToFacMsg_[varToFacMap_[vi][fi]].pruneLabels(labelsToPrune.begin(),labelsToPrune.end());
+         facToVarMsg_[facToVarMap_[fi][vi]].pruneLabels(labelsToPrune.begin(),labelsToPrune.end());
+      }
+      //beliefs_[vi].pruneLabels(labelsToPrune.begin(),labelsToPrune.end());
+   }
+
+
+
+}
+
+template<class GM, class ACC>
+void PBP<GM, ACC>::computeFacToVarMsg(const typename PBP<GM, ACC>::IndexType fi,const typename PBP<GM, ACC>::IndexType vi){
+   FacToVarMsgType & outMsg = facToVarMsg_[facToVarMap_[fi][vi]];
+   OPENGM_CHECK_OP(outMsg.from(),==,fi,"");
+   OPENGM_CHECK_OP(outMsg.to(),  ==,vi,"");
+}
+
+template<class GM, class ACC>
+void PBP<GM, ACC>::computeVarToFacMsg(const typename PBP<GM, ACC>::IndexType vi,const typename PBP<GM, ACC>::IndexType fi){
+
+   VarToFacMsgType & outMsg = varToFacMsg_[varToFacMap_[vi][fi]];
+   OPENGM_CHECK_OP(outMsg.from(),==,vi,"");
+   OPENGM_CHECK_OP(outMsg.to(),  ==,fi,"");
+
+
+   // Variable to factor update:
+   // get  all factor to variable messages
+   // where variable is vi and factor is NOT fi
+   const IndexType nFac=gm_.numberOfFactors(vi);
+   for(IndexType f=0;f<nFac;++f){
+      const IndexType otherFi=gm_.factorOfVariable(vi,f);
+      if(otherFi!=fi){
+         // get the other message
+         const FacToVarMsgType & factorToVarMsg = facToVarMsg_[facToVarMap_[otherFi][vi]];
+         OPENGM_CHECK_OP(factorToVarMsg.from(),==,otherFi,"");
+         OPENGM_CHECK_OP(factorToVarMsg.to(),  ==,vi,"");
+      }
+   }
+
+}
+
+
+
+template<class GM, class ACC>      
+void PBP<GM, ACC>::forwardPass(){
+   std::cout<<"start forwardPass\n";
+
+   for(IndexType node=0;node<gm_.numberOfVariables();++node){
+
+      // get node with smallest confusing set / highest priority
+      const IndexType vi=queue_.getAndRemoveHighestPriorityVi();
+      // check that vi IS NOT commited
+      OPENGM_CHECK(isCommited_[vi]==false,"internal error");
+
+      // make the node commited
+      // and make timstamp
+      isCommited_[vi]=true;
+      forwardTime_[node]=vi;
+
+
+      // apply label pruning to this node
+      this->applyLabelPruning(vi);
+
+      // send to all uncommited neighbors 
+      const IndexType nFac=gm_.numberOfFactors(vi);
+      for(IndexType f=0;f<nFac;++f){
+         const IndexType fi=gm_.factorOfVariable(vi,f);
+         OPENGM_CHECK_OP(gm_[fi].numberOfVariables(),<=,2,"order higher than 2 are not yet implemented");
+         if(gm_[fi].numberOfVariables()==1){
+            // they are handled only once
+         }
+         else if(gm_[fi].numberOfVariables()==2) {
+            const IndexType otherVi= (gm_[fi].variableIndex(0)==vi ?   gm_[fi].variableIndex(1) : gm_[fi].variableIndex(0));
+
+            // only send messages if other fi IS NOT commited
+            if(isCommited_[otherVi]==false){
+               // send message  vi -> fi  ; fi -> otherVi
+               std::cout<<"send vi("<<vi<<") -> fi("<<fi<<") -> ovi("<<otherVi<<")\n";
+               this->computeVarToFacMsg(vi,fi);
+               this->computeFacToVarMsg(fi,otherVi);
+            }
+         }
+      }
+
+      // update beliefs for all uncommited neigbours
+      for(IndexType nv=0;nv<viAdj_[vi].size();++nv){
+         const IndexType otherVi=viAdj_[vi][nv];
+         // only send messages if other fi IS NOT commited
+         if(isCommited_[otherVi]==false){
+            this->updateBeliefAndPriority(otherVi);
+         }
+      }
+   }
+}
+
+template<class GM, class ACC>
+void PBP<GM, ACC>::backwardPass(){
+   std::cout<<"start backwardPass\n";
+
+   for(IndexType node=0;node<gm_.numberOfVariables();++node){
+      // get nodes from timestamp
+      const IndexType vi=forwardTime_[gm_.numberOfVariables()-1-node];
+      // check that vi IS commited
+      OPENGM_CHECK(isCommited_[vi]==true,"internal error");
+
+      // make the node commited
+      // and make timstamp
+      isCommited_[vi]=false;
+      // re-add to queue_
+      queue_.addVi(vi,confusigSetSize_[vi]);
+
+      const IndexType nFac=gm_.numberOfFactors(vi);
+      for(IndexType f=0;f<nFac;++f){
+         const IndexType fi=gm_.factorOfVariable(vi,f);
+         if(gm_[fi].numberOfVariables()==1){
+            // they are handled only once
+         }
+         else if(gm_[fi].numberOfVariables()==2) {
+            const IndexType otherVi= (gm_[fi].variableIndex(0)==vi ?   gm_[fi].variableIndex(1) : gm_[fi].variableIndex(0));
+            // only send messages if other fi IS commited
+            if(isCommited_[otherVi]==true){
+               // send message  vi -> fi  ; fi -> otherVi
+               std::cout<<"send vi("<<vi<<") -> fi("<<fi<<") -> ovi("<<otherVi<<")\n";
+               this->computeVarToFacMsg(vi,fi);
+               this->computeFacToVarMsg(fi,otherVi);
+            }
+         }
+      }
+      // update beliefs for all commited neigbours
+      for(IndexType nv=0;nv<viAdj_[vi].size();++nv){
+         const IndexType otherVi=viAdj_[vi][nv];
+         // only send messages if other fi IS  commited
+         if(isCommited_[otherVi]==true){
+            this->updateBeliefAndPriority(otherVi);
+         }
+      }
+   }
+}
+
+template<class GM, class ACC>
+template<class VisitorType>
+InferenceTermination PBP<GM,ACC>::infer
+(
+   VisitorType& visitor
+)
+{
+   visitor.begin(*this);
+   
+   for(size_t s=0;s<param_.steps_;++s){
+      std::cout<<"iteration s="<<s<<"\n";
+      this->forwardPass();
+      this->backwardPass();
+
+   }
+
+   visitor.end(*this);
+   return NORMAL;
+}
+
 
    
 template<class GM, class ACC>
 inline void 
-ICM<GM,ACC>::setStartingPoint
+PBP<GM,ACC>::setStartingPoint
 (
-   typename std::vector<typename ICM<GM,ACC>::LabelType>::const_iterator begin
+   typename std::vector<typename PBP<GM,ACC>::LabelType>::const_iterator begin
 ) {
 
 }
    
 template<class GM, class ACC>
 inline std::string
-ICM<GM, ACC>::name() const
+PBP<GM, ACC>::name() const
 {
-   return "ICM";
+   return "PBP";
 }
 
 template<class GM, class ACC>
-inline const typename ICM<GM, ACC>::GraphicalModelType&
-ICM<GM, ACC>::graphicalModel() const
+inline const typename PBP<GM, ACC>::GraphicalModelType&
+PBP<GM, ACC>::graphicalModel() const
 {
    return gm_;
 }
   
 template<class GM, class ACC>
 inline InferenceTermination
-ICM<GM,ACC>::infer()
+PBP<GM,ACC>::infer()
 {
    EmptyVisitorType v;
    return infer(v);
 }
 
   
-template<class GM, class ACC>
-template<class VisitorType>
-InferenceTermination ICM<GM,ACC>::infer
-(
-   VisitorType& visitor
-)
-{
-   visitor.begin(*this,movemaker_.value(), movemaker_.value());
-   /////////////////////////
-   // INFERENCE CODE HERE //
-   /////////////////////////
-   visitor.end(*this,movemaker_.value(), movemaker_.value());
-   return NORMAL;
-}
+
 
 template<class GM, class ACC>
 inline InferenceTermination
-ICM<GM,ACC>::arg
+PBP<GM,ACC>::arg
 (
       std::vector<LabelType>& x,
       const size_t N
 ) const
 {
-
+   x.resize(gm_.numberOfVariables(),0);
+   return NORMAL;
 }
 
 } // namespace opengm
 
-#endif // #ifndef OPENGM_ICM_HXX
+#endif // #ifndef OPENGM_PBP_HXX
---
+++ src/unittest/inference/test_pbp.cxx
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <vector>
+#include <set>
+#include <functional>
+
+#include <opengm/graphicalmodel/graphicalmodel.hxx>
+#include <opengm/operations/adder.hxx>
+#include <opengm/operations/multiplier.hxx>
+#include <opengm/operations/minimizer.hxx>
+#include <opengm/operations/maximizer.hxx>
+
+#include <opengm/inference/pbp.hxx>
+
+#include <opengm/unittests/blackboxtester.hxx>
+#include <opengm/unittests/blackboxtests/blackboxtestgrid.hxx>
+#include <opengm/unittests/blackboxtests/blackboxtestfull.hxx>
+#include <opengm/unittests/blackboxtests/blackboxteststar.hxx>
+
+int main() {
+
+
+
+
+
+   typedef opengm::GraphicalModel<double, opengm::Adder> SumGmType;
+
+   std::vector<size_t> confusingSet(100,256);
+
+   SumGmType gm(SumGmType::SpaceType(confusingSet.begin(),confusingSet.end()));
+
+
+
+   opengm::detail_pbp::Priority<SumGmType> dpq(gm);
+
+
+   confusingSet[10]=2;
+   dpq.changeConfusingSetSize(10,256,2);
+   OPENGM_TEST_EQUAL( dpq.highestPriorityVi(),10);
+
+   confusingSet[55]=1;
+   dpq.changeConfusingSetSize(55,256,1);
+   OPENGM_TEST_EQUAL( dpq.highestPriorityVi(),55);
+
+
+   confusingSet[55]=3;
+   dpq.changeConfusingSetSize(56,256,3);
+   OPENGM_TEST_EQUAL( dpq.highestPriorityVi(),55);
+
+   OPENGM_TEST_EQUAL( dpq.getAndRemoveHighestPriorityVi(),55);
+   OPENGM_TEST_EQUAL( dpq.getAndRemoveHighestPriorityVi(),10);
+   OPENGM_TEST_EQUAL( dpq.getAndRemoveHighestPriorityVi(),56);
+
+
+
+
+   
+   typedef opengm::GraphicalModel<double, opengm::Adder> SumGmType;
+   typedef opengm::BlackBoxTestGrid<SumGmType> SumGridTest;
+   opengm::InferenceBlackBoxTester<SumGmType> sumTester;
+   sumTester.addTest(new SumGridTest(10, 10, 4, false, true, SumGridTest::RANDOM, opengm::PASS, 1));
+
+
+   
+   std::cout << "ICM  Tests ..." << std::endl;
+   {
+      std::cout << "  * Minimization/Adder  ..." << std::endl;
+      typedef opengm::PBP<SumGmType, opengm::Minimizer> PBP;
+      PBP::Parameter para;
+      sumTester.test<PBP>(para);
+      std::cout << " OK!"<<std::endl;
+   } 
+   
+}
+
+
+
