4c1819b2e85047a9a669117649d7d828efbd4af4
DerThorsten <thorsten.beier@wir.uni-heidelberg.de>
2013-11-27 13:15:08 +0100

    implemented new visitors

--- include/opengm/inference/new_visitors/new_visitors.hxx
+++ include/opengm/inference/new_visitors/new_visitors.hxx
@@ -1,22 +1,110 @@
+#include <iostream>
+
+template<class INFERENCE>
+class EmptyVisitor{
+public:
+	EmptyVisitor(){
+	}
+	void begin(INFERENCE & inf){
+	}
+	bool operator()(INFERENCE & inf){
+		return true;
+	}
+	void end(INFERENCE & inf){
+	}
+};
+
 
 
 template<class INFERENCE>
-class VisitorInterface{
+class VerboseVisitor{
+public:
+	VerboseVisitor(){
+	}
+	void begin(INFERENCE & inf){
+		std::cout<<"value "<<inf.value()<<" bound "<<inf.bound()<<"\n";
+	}
+	bool operator()(INFERENCE & inf){
+		std::cout<<"value "<<inf.value()<<" bound "<<inf.bound()<<"\n";
+		return true;
+	}
+	void end(INFERENCE & inf){
+		std::cout<<"value "<<inf.value()<<" bound "<<inf.bound()<<"\n";
+	}
+};
 
+
+
+template<class INFERENCE>
+class TimingVisitor{
+public:
+	typedef typename  INFERENCE::ValueType ValueType;
 	
-	void begin(){
-
+	TimingVisitor() 
+	:
+		iteration_(0)
+		times_(),
+		values_(),
+		bounds_(),
+		iterations_(),
+		timer_()
+	{
+		timer_.tic();
 	}
 
-	bool visit(){
+	void begin(INFERENCE & inf){
+		// stop timer
+		timer_.toc();
+		// store values
+		const ValueType val=inf.value();
+		const ValueType bound=inf.bound();
+        times_.push_back(timer_.elapsedTime());
+        values_.push_back(values_);
+        bounds_.push_back(bounds_);
 
+        std::cout<<"value "<<val<<" bound "<<bound<<"\n";
+
+        ++iteration_;
+		// restart timer
+		timer_.tic():
 	}
 
-	void end(){
+	bool operator()(INFERENCE & inf){
+		// stop timer
+		timer_.toc();
+		// store values
+		const ValueType val=inf.value();
+		const ValueType bound=inf.bound();
+        times_.push_back(timer_.elapsedTime());
+        values_.push_back(values_);
+        bounds_.push_back(bounds_);
 
+        std::cout<<"value "<<val<<" bound "<<bound<<"\n";
+
+        ++iteration_;
+		// restart timer
+		timer_.tic():
+		return true;
 	}
 
-	void logg
+	void end(INFERENCE & inf){
+		// stop timer
+		timer_.toc();
+		// store values
+		const ValueType val=inf.value();
+		const ValueType bound=inf.bound();
+        times_.push_back(timer_.elapsedTime());
+        values_.push_back(values_);
+        bounds_.push_back(bounds_);
 
+        std::cout<<"value "<<val<<" bound "<<bound<<"\n";
 
+	}
+private:
+	iteration_;
+	std::vector<float  > times_;
+	std::vector<float  > values_;
+	std::vector<float  > bounds_;
+	std::vector<size_t > iterations_;
+	opengm::Timer timer_;
 };
