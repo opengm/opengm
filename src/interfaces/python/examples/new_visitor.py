b324638cb0536b11778c2a6aa131be56243cba64
DerThorsten <thorsten.beier@iwr.uni-heidelberg.de>
2013-12-06 15:04:06 +0100

    working on new vistors (icm works)

--- src/interfaces/python/examples/new_visitor.py
+++ src/interfaces/python/examples/new_visitor.py
@@ -1,8 +1,8 @@
 import opengm
 import numpy
 
-unaries=numpy.random.rand(1000, 1000,4)
-potts=opengm.PottsFunction([4,4],0.0,0.2)
+unaries=numpy.random.rand(1000, 1000,7)
+potts=opengm.PottsFunction([7,7],0.0,0.2)
 gm=opengm.grid2d2Order(unaries=unaries,regularizer=potts)
 
 
@@ -11,7 +11,7 @@ gm=opengm.grid2d2Order(unaries=unaries,regularizer=potts)
 
 inf=opengm.inference.Icm(gm,parameter=opengm.InfParam())
 
-v=inf.timingVisitor(visitNth=1000,timeLimit=4)
+v=inf.timingVisitor(visitNth=1000,timeLimit=7)
 inf.infer(v)
 
 #print v.getTimes()
