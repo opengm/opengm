d3b506185867b38e880f5675ff07c1a584bd6a56
joergkappes <kappes@math.uni-heidelberg.de>
2014-05-16 17:08:55 +0200

    start hammingloss

--- include/opengm/learning/loss/hammingloss.hxx
+++ include/opengm/learning/loss/hammingloss.hxx
@@ -1,1 +1,35 @@
-//todo Joerg
+#include "opengm/functions/explicit_function.hxx"
+
+
+class HammingLoss{
+public:
+
+   template<class IT1, class IT2>
+   double loss(IT1 labelBegin, IT1 labelEnd, IT2 GTBegin,IT2 GTEnd) const;
+  
+   template<class GM, class IT>
+   void addLoss(GM& gm, IT GTBegin) const;
+private:
+};
+
+template<class IT1, class IT2>
+double HammingLoss::loss(IT1 labelBegin, const IT1 labelEnd, IT2 GTBegin, const IT2 GTEnd) const
+{
+   double loss = 0;
+   for(; labelBegin!= labelEnd; ++labelBegin, ++GTBegin){
+      if(*labelBegin != *GTBegin){
+         loss += 1.0;
+      }
+   }
+}
+
+template<class GM, class IT>
+void HammingLoss::addLoss(GM& gm, IT GTBegin) const
+{
+
+   for(typename GM::IndexType i=0; i<gm.numberOfVariables(); ++i){
+      typename GM::LabelType numL = gm.numberOFLabels(i);
+      //todo add functions and factors
+   }
+}
+  
