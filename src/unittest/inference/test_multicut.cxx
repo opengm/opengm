db43dcb6f2a4f12155d54bc1ddc782849fa1a8a0
joergkappes <kappes@math.uni-heidelberg.de>
2014-08-01 10:06:19 +0200

    Add support for asymetric multiway cuts as described in (Asymmetric Cuts: Joint Image Labeling and Partitioning), GCPR 2014

--- src/unittest/inference/test_multicut.cxx
+++ src/unittest/inference/test_multicut.cxx
@@ -15,6 +15,9 @@
 #include <opengm/unittests/blackboxtests/blackboxtestfull.hxx>
 #include <opengm/unittests/blackboxtests/blackboxteststar.hxx>
 
+
+#include <opengm/functions/potts.hxx>
+
 #ifdef WITH_CPLEX
 #include <opengm/inference/multicut.hxx>
 
@@ -103,6 +106,107 @@ struct MulticutTest
     }
   }
    
+   void testAsymetricMultiwayCut(){
+      typedef double                                                                 ValueType;          // type used for values
+      typedef size_t                                                                 IndexType;          // type used for indexing nodes and factors (default : size_t)
+      typedef size_t                                                                 LabelType;          // type used for labels (default : size_t)
+      typedef opengm::Adder                                                          OpType;             // operation used to combine terms
+      typedef opengm::ExplicitFunction<ValueType,IndexType,LabelType>                ExplicitFunction;   // shortcut for explicite function 
+      typedef opengm::PottsFunction<ValueType,IndexType,LabelType>                   PottsFunction;      // shortcut for Potts function
+      typedef opengm::meta::TypeListGenerator<ExplicitFunction,PottsFunction>::type  FunctionTypeList;   // list of all function the model cal use (this trick avoids virtual methods) - here only one
+      typedef opengm::DiscreteSpace<IndexType, LabelType>                            SpaceType;          // type used to define the feasible statespace
+      typedef opengm::GraphicalModel<ValueType,OpType,FunctionTypeList,SpaceType>    Model;              // type of the model
+      typedef Model::FunctionIdentifier                                              FunctionIdentifier; // type of the function identifier
+      typedef opengm::Multicut<Model, opengm::Minimizer>                             Multicut;
+
+      // Build empty Model
+      LabelType numLabel = 2;
+      size_t N=8,M=8;
+      std::vector<LabelType> numbersOfLabels(N*M,numLabel);
+      Model gm(SpaceType(numbersOfLabels.begin(), numbersOfLabels.end()));
+      
+      // Add 1st order functions and factors to the model
+      for(IndexType variable = 0; variable < gm.numberOfVariables(); ++variable) {
+         // construct 1st order function
+         const LabelType shape[] = {gm.numberOfLabels(variable)};
+         ExplicitFunction f(shape, shape + 1);
+         f(0) = (rand()%10000) / 10000.0 - 0.5;
+         f(1) = (rand()%10000) / 10000.0 - 0.5;
+         // add function
+         FunctionIdentifier id = gm.addFunction(f);
+         // add factor
+         IndexType variableIndex[] = {variable};
+         gm.addFactor(id, variableIndex, variableIndex + 1);
+      }
+      // add 2nd order functions for all variables neighbored on the grid
+      {
+         // add a potts function to the model  
+         IndexType vars[]  = {0,1}; 
+         for(IndexType n=0; n<N;++n){
+            for(IndexType m=0; m<M;++m){
+               vars[0] = n + m*N;
+               if(n+1<N){ //check for right neighbor
+                  vars[1] =  (n+1) + (m  )*N;
+                  OPENGM_ASSERT(vars[0] < vars[1]); // variables need to be ordered! 
+                  double b = (rand()%10000) / 10000.0 - 0.5;
+                  PottsFunction potts(numLabel, numLabel, 0.0, b);
+                  FunctionIdentifier pottsid = gm.addFunction(potts);
+                  gm.addFactor(pottsid, vars, vars + 2);
+               } 
+               if(m+1<M){ //check for lower neighbor
+                  vars[1] =  (n  ) + (m+1)*N; 
+                  OPENGM_ASSERT(vars[0] < vars[1]); // variables need to be ordered!
+                  double b = (rand()%10000) / 10000.0 - 0.5;
+                  PottsFunction potts(numLabel, numLabel, 0.0, b);
+                  FunctionIdentifier pottsid = gm.addFunction(potts);
+                  gm.addFactor(pottsid, vars, vars + 2);
+               }
+            }
+         }
+      }
+
+      Multicut::Parameter para;
+      para.allowCutsWithin_.resize(numLabel, false);
+      para.allowCutsWithin_[0] = true;
+      Multicut mc(gm,para);  
+      mc.infer();
+      std::vector<size_t> arg;
+      std::vector<size_t> seg = mc.getSegmentation();
+      mc.arg(arg);
+   
+      std::cout <<std::endl;
+      for(size_t n=0; n<N; ++n){
+         for(size_t m=0; m<M; ++m){
+            std::cout <<arg[n+m*N]<< " ";
+         }
+         std::cout <<std::endl;
+      }
+      std::cout <<std::endl;
+      for(size_t n=0; n<N; ++n){
+         for(size_t m=0; m<M; ++m){
+            std::cout <<seg[n+m*N]<< " ";
+         }
+         std::cout <<std::endl;
+      }
+
+
+      std::vector<size_t> map(N*M);
+      for(size_t n=0; n<N; ++n){
+         for(size_t m=0; m<M; ++m){
+            map[seg[n+m*N]] = arg[n+m*N];
+         }
+      }
+      for(size_t n=0; n<N; ++n){
+         for(size_t m=0; m<M; ++m){
+            OPENGM_ASSERT(map[seg[n+m*N]] == arg[n+m*N]);
+         }
+      }
+      
+
+
+      
+   }
+
 
   void run(){
     std::cout <<std::endl;
@@ -129,7 +233,10 @@ struct MulticutTest
     testException<opengm::Maximizer,opengm::Multiplier>();
 
     std::cout << "  * Start Black-Box Tests for Sum-Prod..."<<std::endl;
-    testException<opengm::Integrator,opengm::Multiplier>();
+    testException<opengm::Integrator,opengm::Multiplier>(); 
+ 
+    std::cout << "  * Start Test for Asymetric Multiway Cut ..."<<std::endl;
+    testAsymetricMultiwayCut();
   }
 };
 #endif
