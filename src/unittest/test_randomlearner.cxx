a747fd37f47646372ea974cd7360fa131828aecc
joergkappes <kappes@math.uni-heidelberg.de>
2014-11-19 15:42:00 +0100

    write test for grid search learning(todo: rename this file)

--- src/unittest/test_randomlearner.cxx
+++ src/unittest/test_randomlearner.cxx
@@ -14,8 +14,34 @@
 #include <opengm/learning/dataset/testdataset.hxx>
 
 
+//*************************************
+typedef double ValueType;
+typedef size_t IndexType;
+typedef size_t LabelType; 
+typedef opengm::meta::TypeListGenerator<opengm::ExplicitFunction<ValueType,IndexType,LabelType>, opengm::functions::learnable::LPotts<ValueType,IndexType,LabelType> >::type FunctionListType;
+typedef opengm::GraphicalModel<ValueType,opengm::Adder, FunctionListType, opengm::DiscreteSpace<IndexType,LabelType> > GM; 
+typedef opengm::datasets::TestDataset<GM> DS;
+typedef opengm::learning::HammingLoss     LOSS;
+typedef opengm::ICM<GM,opengm::Minimizer> INF;
+
+//*************************************
+
 
 int main() {
-   std::cout << " Includes are fine :-) " << std::endl;
+   std::cout << " Includes are fine :-) " << std::endl; 
+  
+   DS dataset;
+   std::cout << "Dataset includes " << dataset.getNumberOfModels() << " instances and has " << dataset.getNumberOfParameters() << " parameters."<<std::endl; 
+  
+
+   opengm::learning::GridSearchLearner<DS,LOSS>::Parameter para;
+   para.parameterUpperbound_.resize(1,1);
+   para.parameterLowerbound_.resize(1,0);
+   para.testingPoints_.resize(1,10);
+   opengm::learning::GridSearchLearner<DS,LOSS> learner(dataset,para);
+
+
+   INF::Parameter infPara;
+   learner.learn<INF>(infPara);
 
 }
