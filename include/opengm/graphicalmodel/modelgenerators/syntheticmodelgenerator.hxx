ee2ea22808815ace1b452446c85256cbceb5ca1d
Christopher Pommrenke <christopher@pommrenke.com>
2014-11-26 14:22:28 +0100

    added higher order grid black box test

--- include/opengm/graphicalmodel/modelgenerators/syntheticmodelgenerator.hxx
+++ include/opengm/graphicalmodel/modelgenerators/syntheticmodelgenerator.hxx
@@ -4,16 +4,317 @@
 
 /// \cond HIDDEN_SYMBOLS
 
-#include <cstdlib>
 #include <vector>
-#include <cstdlib>
-#include <set>
-#include <functional>
+#include <map>
 
 #include <opengm/graphicalmodel/graphicalmodel.hxx>
+#include <opengm/functions/explicit_function.hxx>
+#include <opengm/functions/constant.hxx>
+#include <opengm/functions/potts.hxx>
+#include <opengm/functions/absolute_difference.hxx>
+#include <opengm/utilities/indexing.hxx>
 #include <opengm/utilities/random.hxx>
 
 namespace opengm {
+
+   template<class GM>
+   class SyntheticModelGenerator {
+   public:
+      // typedefs
+      typedef GM GraphicalModelType;
+      typedef typename GM::ValueType ValueType;
+      typedef typename GM::IndexType IndexType;
+      typedef typename GM::LabelType LabelType;
+
+      typedef typename GM::FunctionIdentifier FunctionIdentifier;
+
+      typedef opengm::ExplicitFunction<ValueType, IndexType, LabelType>           ExplicitFunctionType;
+      typedef opengm::ConstantFunction<ValueType, IndexType, LabelType>           ConstantFunctionType;
+      typedef opengm::PottsFunction<ValueType, IndexType, LabelType>              PottsFunctionType;
+      typedef opengm::AbsoluteDifferenceFunction<ValueType, IndexType, LabelType> AbsoluteDifferenceFunctionType;
+
+      typedef RandomUniform<double> URandomGenerator;
+      typedef RandomUniform<int>    IRandomGenerator;
+
+      struct Parameter {
+         // typedefs
+         enum FunctionTypes {CONSTF, URANDOM, IRANDOM, POTTS, L1};
+         typedef std::pair<ValueType, ValueType> MinMax;
+
+         // member
+         LabelType                            maxNumberOfStates_;
+         bool                                 randomNumberOfStates_;
+         std::vector<FunctionTypes>           functionsType_;
+         std::vector<MinMax>                  functionsMinMax_;
+         std::vector<IndexType>               functionsOrder_;
+         std::vector<bool>                    sharedFunctions_;
+         std::vector<std::vector<ValueType> > functionsParameter_;
+
+         // constructor
+         Parameter(const LabelType maxNumberOfStatesIn = 2, const bool randomNumberOfStatesIn = false);
+
+         // check parameter consistency
+         bool sanityCheck() const;
+      };
+
+      // static public methods
+      static GraphicalModelType buildGrid(const size_t id, const IndexType width, const IndexType height, const Parameter& parameter);
+      static GraphicalModelType buildFull(const size_t id, const IndexType numberOfVariables, const Parameter& parameter);
+      static GraphicalModelType buildStar(const size_t id, const IndexType numberOfVariables, const Parameter& parameter);
+   protected:
+      // static protected methods
+      template<class IRANDOM_GENERATOR>
+      static GraphicalModelType initGM(const IndexType numberOfVariables, IRANDOM_GENERATOR& integerRandomGenerator, const Parameter& parameter);
+      template<class SHAPE_ITERATOR, class URANDOM_GENERATOR, class IRANDOM_GENERATOR>
+      static FunctionIdentifier addFunction(GraphicalModelType& gm, const typename Parameter::FunctionTypes functionType, const std::vector<ValueType>& functionsParameter, SHAPE_ITERATOR shapeBegin, SHAPE_ITERATOR shapeEnd, URANDOM_GENERATOR& uniformRandomGenerator, IRANDOM_GENERATOR& integerRandomGenerator);
+   };
+
+   template <class GM>
+   inline SyntheticModelGenerator<GM>::Parameter::Parameter(
+         const LabelType maxNumberOfStatesIn, const bool randomNumberOfStatesIn)
+         : maxNumberOfStates_(maxNumberOfStatesIn),
+           randomNumberOfStates_(randomNumberOfStatesIn), functionsType_(),
+           functionsMinMax_(), functionsOrder_(), sharedFunctions_(),
+           functionsParameter_() {
+   }
+
+   template <class GM>
+   inline bool SyntheticModelGenerator<GM>::Parameter::sanityCheck() const {
+      return (functionsType_.size() == functionsMinMax_.size()) &&
+            (functionsMinMax_.size() == functionsOrder_.size()) &&
+            (functionsOrder_.size() == sharedFunctions_.size()) &&
+            (sharedFunctions_.size() == functionsParameter_.size());
+   }
+
+   template <class GM>
+   inline typename SyntheticModelGenerator<GM>::GraphicalModelType SyntheticModelGenerator<GM>::buildGrid(const size_t id, const IndexType width, const IndexType height, const Parameter& parameter) {
+      OPENGM_ASSERT(parameter.sanityCheck());
+
+      URandomGenerator uniformRandomGenerator(0.0, 1.0, id);
+      IRandomGenerator integerRandomGenerator(0, 1, id);
+
+      // init gm
+      // TODO: width * height might overflow hence add overflow test (not trivial)
+      GraphicalModelType gm = initGM(width * height, integerRandomGenerator, parameter);
+
+      // build grid structure
+      std::vector<LabelType> shape(1, parameter.maxNumberOfStates_);
+      std::vector<IndexType> variables(1, 0);
+      FunctionIdentifier functionId;
+      std::map<std::vector<LabelType>, FunctionIdentifier> sharedFunctionMap;
+
+      bool newFunction = true;
+
+      for(size_t i = 0; i < parameter.functionsType_.size(); ++i) {
+         OPENGM_ASSERT(parameter.functionsOrder_[i] <= width);
+         OPENGM_ASSERT(parameter.functionsOrder_[i] <= height);
+
+         // set new bounds for random generators
+         uniformRandomGenerator.setLow(parameter.functionsMinMax_[i].first);
+         uniformRandomGenerator.setHigh(parameter.functionsMinMax_[i].second);
+         integerRandomGenerator.setLow(parameter.functionsMinMax_[i].first);
+         integerRandomGenerator.setHigh(parameter.functionsMinMax_[i].second);
+
+         // resize shape
+         if(parameter.randomNumberOfStates_) {
+            shape.resize(parameter.functionsOrder_[i]);
+         } else {
+            shape.resize(parameter.functionsOrder_[i], parameter.maxNumberOfStates_);
+         }
+
+         // resize variables
+         variables.resize(parameter.functionsOrder_[i]);
+
+         for(IndexType j = 0; j < width; ++j) {
+            for(IndexType k = 0; k < height; ++k) {
+               if(j < width - parameter.functionsOrder_[i] + 1) {
+                  // horizontal
+                  IndexType variableOffset = j + (k * width);
+                  for(IndexType l = 0; l < parameter.functionsOrder_[i]; ++l) {
+                     if(parameter.randomNumberOfStates_) {
+                        shape[l] = gm.numberOfLabels(variableOffset + l);
+                     }
+                     variables[l] = variableOffset + l;
+                  }
+                  if(parameter.sharedFunctions_[i]) {
+                     const typename std::map<std::vector<LabelType>, FunctionIdentifier>::const_iterator position = sharedFunctionMap.find(shape);
+                     if(position != sharedFunctionMap.end()) {
+                        // use shared function
+                        newFunction = false;
+                        functionId = position->second;
+                     } else {
+                        // use new function
+                        newFunction = true;
+                     }
+                  } else {
+                     newFunction = true;
+                  }
+
+                  if(newFunction) {
+                     // add new function
+                     functionId = addFunction(gm, parameter.functionsType_[i], parameter.functionsParameter_[i], shape.begin(), shape.end(), uniformRandomGenerator, integerRandomGenerator);
+                     if(parameter.sharedFunctions_[i]) {
+                        sharedFunctionMap[shape] = functionId;
+                     }
+                  }
+
+                  // add horizontal factor
+                  gm.addFactor(functionId, variables.begin(), variables.end());
+               }
+
+               if((k < height - parameter.functionsOrder_[i] + 1) && (parameter.functionsOrder_[i] > 1)) {
+                  // vertical (only if parameter.functionsOrder_[i] > 1 otherwise unary factors would be added twice)
+                  IndexType variableOffset = j + (k * width);
+                  for(IndexType l = 0; l < parameter.functionsOrder_[i]; ++l) {
+                     if(parameter.randomNumberOfStates_) {
+                        shape[l] = gm.numberOfLabels(variableOffset + (l * width));
+                     }
+                     variables[l] = variableOffset + (l * width);
+                  }
+                  if(parameter.sharedFunctions_[i]) {
+                     const typename std::map<std::vector<LabelType>, FunctionIdentifier>::const_iterator position = sharedFunctionMap.find(shape);
+                     if(position != sharedFunctionMap.end()) {
+                        // use shared function
+                        newFunction = false;
+                        functionId = position->second;
+                     } else {
+                        // use new function
+                        newFunction = true;
+                     }
+                  } else {
+                     newFunction = true;
+                  }
+
+                  if(newFunction) {
+                     // add new function
+                     functionId = addFunction(gm, parameter.functionsType_[i], parameter.functionsParameter_[i], shape.begin(), shape.end(), uniformRandomGenerator, integerRandomGenerator);
+                     if(parameter.sharedFunctions_[i]) {
+                        sharedFunctionMap[shape] = functionId;
+                     }
+                  }
+
+                  // add vertical factor
+                  gm.addFactor(functionId, variables.begin(), variables.end());
+               }
+            }
+         }
+
+         // clear shared function map for next iteration
+         if(parameter.sharedFunctions_[i]) {
+            sharedFunctionMap.clear();
+         }
+      }
+
+      return gm;
+   }
+
+   template <class GM>
+   inline typename SyntheticModelGenerator<GM>::GraphicalModelType SyntheticModelGenerator<GM>::buildFull(const size_t id, const IndexType numberOfVariables, const Parameter& parameter) {
+
+   }
+
+   template <class GM>
+   inline typename SyntheticModelGenerator<GM>::GraphicalModelType SyntheticModelGenerator<GM>::buildStar(const size_t id, const IndexType numberOfVariables, const Parameter& parameter) {
+
+   }
+
+   template <class GM>
+   template<class IRANDOM_GENERATOR>
+   inline typename SyntheticModelGenerator<GM>::GraphicalModelType SyntheticModelGenerator<GM>::initGM(const IndexType numberOfVariables, IRANDOM_GENERATOR& integerRandomGenerator, const Parameter& parameter) {
+      if(parameter.randomNumberOfStates_) {
+         std::vector<typename GM::LabelType> numberOfLabels(numberOfVariables);
+         // generate random integer variables in the range [1, parameter.maxNumberOfStates_ + 1) = [1, parameter.maxNumberOfStates_]
+         // TODO check if parameter.maxNumberOfStates_ + 1 fits in value type of IRANDOM_GENERATOR
+         integerRandomGenerator.setLow(1);
+         integerRandomGenerator.setHigh(parameter.maxNumberOfStates_ + 1);
+
+         // generate number of labels
+         for(size_t i = 0; i < numberOfVariables; i++) {
+            numberOfLabels[i] = integerRandomGenerator();
+         }
+
+         return GM(opengm::DiscreteSpace<typename GM::IndexType,typename GM::LabelType>(numberOfLabels.begin(), numberOfLabels.end()));
+      } else {
+         std::vector<typename GM::LabelType> numberOfLabels(numberOfVariables, parameter.maxNumberOfStates_);
+         return GM(opengm::DiscreteSpace<typename GM::IndexType,typename GM::LabelType>(numberOfLabels.begin(), numberOfLabels.end()));
+      }
+   }
+
+   template <class GM>
+   template<class SHAPE_ITERATOR, class URANDOM_GENERATOR, class IRANDOM_GENERATOR>
+   inline typename SyntheticModelGenerator<GM>::FunctionIdentifier SyntheticModelGenerator<GM>::addFunction(GraphicalModelType& gm, const typename Parameter::FunctionTypes functionType, const std::vector<ValueType>& functionParameter, SHAPE_ITERATOR shapeBegin, SHAPE_ITERATOR shapeEnd, URANDOM_GENERATOR& uniformRandomGenerator, IRANDOM_GENERATOR& integerRandomGenerator) {
+      switch (functionType) {
+         case Parameter::CONSTF: {
+            if(functionParameter.size() > 0) {
+               OPENGM_ASSERT(functionParameter.size() == 1);
+               ConstantFunctionType constantFunction(shapeBegin, shapeEnd, functionParameter[0]);
+               return gm.addFunction(constantFunction);
+            } else {
+               ConstantFunctionType constantFunction(shapeBegin, shapeEnd, uniformRandomGenerator());
+               return gm.addFunction(constantFunction);
+            }
+         }
+         case Parameter::URANDOM: {
+            ExplicitFunctionType function(shapeBegin, shapeEnd);
+            ShapeWalker<SHAPE_ITERATOR> shapeWalker(shapeBegin, function.dimension());
+            if(functionParameter.size() > 0) {
+               OPENGM_ASSERT(functionParameter.size() == function.size());
+               for(IndexType i = 0; i < function.size(); ++i) {
+                  function(shapeWalker.coordinateTuple().begin()) = functionParameter[i];
+                  ++shapeWalker;
+               }
+            } else {
+               for(IndexType i = 0; i < function.size(); ++i) {
+                  function(shapeWalker.coordinateTuple().begin()) = uniformRandomGenerator();
+                  ++shapeWalker;
+               }
+            }
+            return gm.addFunction(function);
+         }
+         case Parameter::IRANDOM: {
+            ExplicitFunctionType function(shapeBegin, shapeEnd);
+            ShapeWalker<SHAPE_ITERATOR> shapeWalker(shapeBegin, function.dimension());
+            if(functionParameter.size() > 0) {
+               OPENGM_ASSERT(functionParameter.size() == function.size());
+               for(IndexType i = 0; i < function.size(); ++i) {
+                  function(shapeWalker.coordinateTuple().begin()) = functionParameter[i];
+                  ++shapeWalker;
+               }
+            } else {
+               for(IndexType i = 0; i < function.size(); ++i) {
+                  function(shapeWalker.coordinateTuple().begin()) = integerRandomGenerator();
+                  ++shapeWalker;
+               }
+            }
+            return gm.addFunction(function);
+         }
+         case Parameter::POTTS: {
+            OPENGM_ASSERT(std::distance(shapeBegin, shapeEnd) == 2);
+            if(functionParameter.size() > 0) {
+               OPENGM_ASSERT(functionParameter.size() == 2);
+               PottsFunctionType function(shapeBegin[0], shapeBegin[1], functionParameter[0], functionParameter[1]);
+               return gm.addFunction(function);
+            } else {
+               PottsFunctionType function(shapeBegin[0], shapeBegin[1], uniformRandomGenerator(), uniformRandomGenerator());
+               return gm.addFunction(function);
+            }
+         }
+         case Parameter::L1: {
+            OPENGM_ASSERT(std::distance(shapeBegin, shapeEnd) == 2);
+            if(functionParameter.size() > 0) {
+               OPENGM_ASSERT(functionParameter.size() == 1);
+               AbsoluteDifferenceFunctionType function(shapeBegin[0], shapeBegin[1], functionParameter[0]);
+               return gm.addFunction(function);
+            } else {
+               AbsoluteDifferenceFunctionType function(shapeBegin[0], shapeBegin[1], uniformRandomGenerator());
+               return gm.addFunction(function);
+            }
+         }
+         default:
+            throw RuntimeError("Unknown function type.");
+      }
+   }
+
 
    template<class GM>
    class SyntheticModelGenerator2
@@ -65,6 +366,7 @@ namespace opengm {
 
       SyntheticModelGenerator2();
       GM buildGrid(const size_t, const size_t, const size_t, const size_t, const Parameter&) const;
+      GM buildHigherOrderGrid(const size_t, const size_t, const size_t, const size_t, const size_t, const Parameter&) const;
       GM buildFull(const size_t, const size_t, const size_t, const Parameter&) const;
       GM buildStar(const size_t, const size_t, const size_t, const Parameter&) const;
 
@@ -298,6 +600,60 @@ namespace opengm {
    }
 
    template<class GM>
+   GM SyntheticModelGenerator2<GM>::buildHigherOrderGrid(
+         const size_t id,
+         const size_t height, const size_t width,
+         const size_t numStates, const size_t order,
+         const Parameter& parameter) const {
+      srand(id);
+      OPENGM_ASSERT(parameter.isConsistent());
+      OPENGM_ASSERT(parameter.functionTypes_.size() == 2);
+      OPENGM_ASSERT(order >= 2);
+      OPENGM_ASSERT(order <= height);
+      OPENGM_ASSERT(order <= width);
+
+      GraphicalModelType gm = getGM(height*width,numStates,parameter.randomNumberOfStates_);
+
+      // add unaries
+      addUnaries(gm, parameter.functionTypes_[0], parameter.functionParameters_[0], parameter.sharedFunctions_[0] && !parameter.randomNumberOfStates_);
+
+      // add higher order factors
+      typename GM::LabelType shape[order];
+      typename GM::IndexType var[order];
+      bool newFunction = true;
+      FunctionIdentifier funcId;
+      for(size_t i = 0; i < height - order + 1; ++i) {
+         for(size_t j = 0; j < width - order + 1; ++j) {
+            size_t v = i + height * j;
+            if(i + 1 < height) {
+               var[0] = v;
+               var[1] = i + 1 + height * j;
+               shape[0] = gm.numberOfLabels(var[0]);
+               shape[1] = gm.numberOfLabels(var[1]);
+               if(newFunction) {
+                  funcId = addFunction(gm, parameter.functionTypes_[1], parameter.functionParameters_[1], shape, shape + 2);
+               }
+               newFunction = !parameter.sharedFunctions_[1] || parameter.randomNumberOfStates_;
+               gm.addFactor(funcId, var, var + 2);
+            }
+            if(j + 1 < width) {
+               var[0] = v;
+               var[1] = i + height * (j + 1);
+               shape[0] = gm.numberOfLabels(var[0]);
+               shape[1] = gm.numberOfLabels(var[1]);
+               if(newFunction) {
+                  funcId = addFunction(gm, parameter.functionTypes_[1], parameter.functionParameters_[1], shape, shape + 2);
+               }
+               newFunction = !parameter.sharedFunctions_[1] || parameter.randomNumberOfStates_;
+               gm.addFactor(funcId, var, var + 2);
+            }
+         }
+      }
+      return gm;
+
+   }
+
+   template<class GM>
    GM SyntheticModelGenerator2<GM>::buildFull
    (
       const size_t id,
@@ -384,4 +740,3 @@ namespace opengm {
 /// \endcond
 
 #endif // #ifndef OPENGM_SYNTHETIC_MODEL_GENERATOR2_HXX
-
---
+++ include/opengm/unittests/blackboxtests/blackboxtesthigherordergrid.hxx
@@ -0,0 +1,164 @@
+#ifndef OPENGM_BLACKBOXTESTHIGHERORDERGRID_HXX_
+#define OPENGM_BLACKBOXTESTHIGHERORDERGRID_HXX_
+
+#include <opengm/opengm.hxx>
+#include <opengm/unittests/test.hxx>
+#include <opengm/graphicalmodel/modelgenerators/syntheticmodelgenerator.hxx>
+#include <opengm/unittests/blackboxtests/blackboxtestbase.hxx>
+
+/// \cond HIDDEN_SYMBOLS
+
+namespace opengm {
+
+   template<class GM> class BlackBoxTestBase;
+
+   template<class GM>
+   class BlackBoxTestHigherOrderGrid : public BlackBoxTestBase<GM>
+   {
+   public:
+      enum BlackBoxFunction
+      {
+         RANDOM, POTTS, IPOTTS, L1
+      };
+      const size_t height_;
+      const size_t width_;
+      const size_t numVar_;
+      const size_t numStates_;
+      const size_t maxOrder_;
+      const bool varStates_;
+      const bool withUnary_;
+      const bool multipleOrders_;
+      const BlackBoxFunction function_;
+      const BlackBoxBehaviour behaviour_;
+      const size_t numTests_;
+      const size_t modelIdOffset_;
+
+      BlackBoxTestHigherOrderGrid(const size_t height, const size_t width, const size_t numStates, const size_t maxOrder, const bool varStates, const bool withUnary, const bool multipleOrders, const BlackBoxFunction f, const BlackBoxBehaviour b, const size_t numTests, const size_t modelIdOffset=0);
+      virtual ~BlackBoxTestHigherOrderGrid();
+
+      virtual std::string infoText();
+      virtual GM getModel(size_t id);
+      virtual size_t numberOfTests();
+      virtual BlackBoxBehaviour behaviour();
+
+   private:
+      std::string functionString(const BlackBoxFunction f) const;
+   };
+
+   template<class GM>
+   inline BlackBoxTestHigherOrderGrid<GM>::BlackBoxTestHigherOrderGrid(const size_t height, const size_t width, const size_t numStates, const size_t maxOrder, const bool varStates, const bool withUnary, const bool multipleOrders, const BlackBoxFunction f, const BlackBoxBehaviour b, const size_t numTests, const size_t modelIdOffset)
+      : height_(height), width_(width), numVar_(height * width), numStates_(numStates), maxOrder_(maxOrder), varStates_(varStates), withUnary_(withUnary), multipleOrders_(multipleOrders), function_(f), behaviour_(b), numTests_(numTests), modelIdOffset_(modelIdOffset) {
+      OPENGM_ASSERT(maxOrder_ >= 2);
+      OPENGM_ASSERT(maxOrder_ <= height_);
+      OPENGM_ASSERT(maxOrder_ <= width_);
+   }
+
+   template<class GM>
+   inline BlackBoxTestHigherOrderGrid<GM>::~BlackBoxTestHigherOrderGrid() {
+
+   }
+
+   template<class GM>
+   inline std::string BlackBoxTestHigherOrderGrid<GM>::infoText() {
+      std::string s = varStates_ ? "<" : "";
+      std::string u = withUnary_ ? " with unary " : " without unary ";
+      std::string o = multipleOrders_ ? "<=" : "";
+      std::stringstream oss;
+      oss << "    - higher order grid model (" << o << maxOrder_ << ") with " << numVar_ << " variables and " << s << numStates_ << " states ";
+      oss << "(functionType = " << functionString(function_) << u << ")";
+      return oss.str();
+   }
+
+   template<class GM>
+   inline GM BlackBoxTestHigherOrderGrid<GM>::getModel(size_t id) {
+      typedef opengm::SyntheticModelGenerator<GM> ModelGeneratorType;
+
+      ModelGeneratorType modelGenerator;
+      typename ModelGeneratorType::Parameter modelGeneratorPara(numStates_, varStates_);
+
+      if(withUnary_) {
+         modelGeneratorPara.functionsType_.push_back(ModelGeneratorType::Parameter::URANDOM);
+         modelGeneratorPara.functionsOrder_.push_back(1);
+         modelGeneratorPara.functionsMinMax_.push_back(typename ModelGeneratorType::Parameter::MinMax(1.0, 2.0));
+         modelGeneratorPara.sharedFunctions_.push_back(false);
+         modelGeneratorPara.functionsParameter_.push_back(std::vector<typename GM::ValueType>());
+      }
+
+      for(size_t i = multipleOrders_ ? 2 : maxOrder_; i <= maxOrder_; ++i) {
+         switch (function_) {
+         case RANDOM: {
+            modelGeneratorPara.functionsType_.push_back(ModelGeneratorType::Parameter::URANDOM);
+            modelGeneratorPara.functionsOrder_.push_back(i);
+            modelGeneratorPara.functionsMinMax_.push_back(typename ModelGeneratorType::Parameter::MinMax(1.0, 2.0));
+            modelGeneratorPara.sharedFunctions_.push_back(false);
+            modelGeneratorPara.functionsParameter_.push_back(std::vector<typename GM::ValueType>());
+            break;
+         }
+         case POTTS: {
+            modelGeneratorPara.functionsType_.push_back(ModelGeneratorType::Parameter::POTTS);
+            modelGeneratorPara.functionsOrder_.push_back(i);
+            modelGeneratorPara.functionsMinMax_.push_back(typename ModelGeneratorType::Parameter::MinMax(1.0, 2.0));
+            modelGeneratorPara.sharedFunctions_.push_back(true);
+
+            std::vector<typename GM::ValueType> functionParameter;
+            functionParameter.push_back(0.0);
+            functionParameter.push_back(3.0);
+            modelGeneratorPara.functionsParameter_.push_back(functionParameter);
+            break;
+         }
+         case IPOTTS: {
+            modelGeneratorPara.functionsType_.push_back(ModelGeneratorType::Parameter::POTTS);
+            modelGeneratorPara.functionsOrder_.push_back(i);
+            modelGeneratorPara.functionsMinMax_.push_back(typename ModelGeneratorType::Parameter::MinMax(1.0, 2.0));
+            modelGeneratorPara.sharedFunctions_.push_back(true);
+
+            std::vector<typename GM::ValueType> functionParameter;
+            functionParameter.push_back(0.0);
+            functionParameter.push_back(-3.0);
+            modelGeneratorPara.functionsParameter_.push_back(functionParameter);
+            break;
+         }
+         case L1: {
+            modelGeneratorPara.functionsType_.push_back(ModelGeneratorType::Parameter::L1);
+            modelGeneratorPara.functionsOrder_.push_back(i);
+            modelGeneratorPara.functionsMinMax_.push_back(typename ModelGeneratorType::Parameter::MinMax(1.0, 2.0));
+            modelGeneratorPara.sharedFunctions_.push_back(true);
+            modelGeneratorPara.functionsParameter_.push_back(std::vector<typename GM::ValueType>(1, 1.0));
+            break;
+         }
+         }
+      }
+
+      return modelGenerator.buildGrid(id + modelIdOffset_, width_, height_, modelGeneratorPara);
+   }
+
+   template<class GM>
+   inline size_t BlackBoxTestHigherOrderGrid<GM>::numberOfTests() {
+      return numTests_;
+   }
+
+   template<class GM>
+   inline BlackBoxBehaviour BlackBoxTestHigherOrderGrid<GM>::behaviour() {
+      return behaviour_;
+   }
+
+   template<class GM>
+   inline std::string BlackBoxTestHigherOrderGrid<GM>::functionString(const BlackBoxFunction f) const {
+      switch (f) {
+      case RANDOM:
+         return "random";
+      case POTTS:
+         return "potts";
+      case IPOTTS:
+         return "negative potts";
+      case L1:
+         return "L1";
+      default:
+         return "";
+      };
+      return "";
+   }
+
+} // namespace opengm
+
+#endif /* OPENGM_BLACKBOXTESTHIGHERORDERGRID_HXX_ */
