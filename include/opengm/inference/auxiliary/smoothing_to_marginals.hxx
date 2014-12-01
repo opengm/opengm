2507e637c9de238939c4902d9d5fab987ad6d576
Bogdan Savchynskyy <homulko@gmail.com>
2014-12-01 18:37:56 +0100

    comment to the new function setSmoothingParametersForMarginals added

--- include/opengm/inference/auxiliary/smoothing_to_marginals.hxx
+++ include/opengm/inference/auxiliary/smoothing_to_marginals.hxx
@@ -10,6 +10,51 @@
 
 namespace opengm{
 
+//! [function setSmoothingParametersForMarginals]
+/// setSmoothingParametersForMarginals - adjusts parameters of smoothing-based algorithms (NesterovAcceleratedGradient and ADSal)
+/// to obtain estimations of sum-prod margonals.
+///
+/// For mathematical details see the papers:
+/// B. Savchynskyy, J. H. Kappes, S. Schmidt, C. Schnörr
+/// A Study of Nesterov's Scheme for Lagrangian Decomposition and MAP Labeling, in CVPR 2011
+/// and
+/// B. Savchynskyy, S. Schmidt, J. H. Kappes, C. Schnörr
+/// Efficient MRF Energy Minimization via Adaptive Diminishing Smoothing, In UAI, 2012, pp. 746-755
+///
+///
+/// Usage examples:
+///
+////* With NesterovAcceleratedGradient: */
+///NesterovAcceleratedGradient<GraphicalModelType,Minimizer>::Parameter params;
+///setSmoothingParametersForMarginals(params,1,1.0);
+///NesterovAcceleratedGradient<GraphicalModelType,Minimizer> solver(gm,params);
+///solver.infer();
+///
+///GraphicalModelType::IndependentFactorType out;
+///for (size_t i=0;i<gm.numberOfVariables();++i)
+///{
+///	solver.marginal(i,out_nest);
+///			  .../* do with the 'out' marginals what you want */
+///}
+///
+////* With ADSal: */
+///ADSal<GraphicalModelType,Minimizer>::Parameter params;
+///setSmoothingParametersForMarginals(params,1,1.0);
+///ADSal<GraphicalModelType,Minimizer> solver(gm,params);
+///solver.infer();
+///GraphicalModelType::IndependentFactorType out;
+///for (size_t i=0;i<gm.numberOfVariables();++i)
+///{
+///	solver.marginal(i,out);
+///			  .../* do with the 'out' marginals what you want */
+///}
+///
+///
+///
+/// Corresponding author: Bogdan Savchynskyy
+///
+///\ingroup inference
+
 template<class PARAMETERS>
 void setSmoothingParametersForMarginals(PARAMETERS& params,
 		 	 	 	 	 	 	 	 	size_t numIterations,
