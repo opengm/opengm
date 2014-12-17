05ec209153cf88f77fd894437ee4741374198470
Jan Funke <funke@ini.ch>
2014-12-17 14:43:12 +0100

    updated test_learning

--- src/unittest/test_learning.cxx
+++ src/unittest/test_learning.cxx
@@ -6,6 +6,7 @@
 #include <opengm/operations/adder.hxx>
 #include <opengm/operations/minimizer.hxx>
 #include <opengm/inference/external/trws.hxx>
+#include <opengm/inference/lpgurobi.hxx>
 #include <opengm/utilities/metaprogramming.hxx>
 
 #include <opengm/functions/learnable/lpotts.hxx>
@@ -23,44 +24,60 @@ typedef size_t IndexType;
 typedef size_t LabelType; 
 typedef opengm::meta::TypeListGenerator<opengm::ExplicitFunction<ValueType,IndexType,LabelType>, opengm::functions::learnable::LPotts<ValueType,IndexType,LabelType>, opengm::functions::learnable::SumOfExperts<ValueType,IndexType,LabelType> >::type FunctionListType;
 typedef opengm::GraphicalModel<ValueType,opengm::Adder, FunctionListType, opengm::DiscreteSpace<IndexType,LabelType> > GM; 
-//typedef opengm::datasets::TestDataset<GM>  DS;
-//typedef opengm::datasets::TestDataset2<GM> DS2;
 typedef opengm::learning::HammingLoss     LOSS;
-typedef opengm::external::TRWS<GM>        INF;
+typedef opengm::LPGurobi<GM,opengm::Minimizer> INF;
 typedef opengm::datasets::TestDataset1<GM,LOSS> DS1;
 typedef opengm::datasets::TestDataset2<GM,LOSS> DS2;
+typedef opengm::datasets::TestDatasetSimple<GM,LOSS> DSS;
 
 //*************************************
 
 
 int main() {
    std::cout << " Includes are fine :-) " << std::endl; 
+
    {
-      DS1 dataset;
-      std::cout << "Dataset includes " << dataset.getNumberOfModels() << " instances and has " << dataset.getNumberOfWeights() << " parameters."<<std::endl;
-      
-      
-      opengm::learning::StructMaxMargin<DS1,LOSS>::Parameter para;
-      opengm::learning::StructMaxMargin<DS1,LOSS> learner(dataset,para);
-      
-      
-      INF::Parameter infPara;
-      learner.learn<INF>(infPara);
-      
+	  DSS dataset(1);
+	  std::cout << "Dataset includes " << dataset.getNumberOfModels() << " instances and has " << dataset.getNumberOfWeights() << " parameters."<<std::endl;
+	  
+	  
+	  opengm::learning::StructMaxMargin<DSS,LOSS>::Parameter para;
+	  opengm::learning::StructMaxMargin<DSS,LOSS> learner(dataset,para);
+	  
+	  
+	  INF::Parameter infPara;
+	  infPara.integerConstraint_ = true;
+	  learner.learn<INF>(infPara);
    }
 
    {
-      DS2 dataset;
-      std::cout << "Dataset includes " << dataset.getNumberOfModels() << " instances and has " << dataset.getNumberOfWeights() << " parameters."<<std::endl;
-      
-      
-      opengm::learning::StructMaxMargin<DS2,LOSS>::Parameter para;
-      opengm::learning::StructMaxMargin<DS2,LOSS> learner(dataset,para);
-      
-      
-      INF::Parameter infPara;
-      learner.learn<INF>(infPara);
+	  DS1 dataset(1);
+	  std::cout << "Dataset includes " << dataset.getNumberOfModels() << " instances and has " << dataset.getNumberOfWeights() << " parameters."<<std::endl;
+	  
+	  
+	  opengm::learning::StructMaxMargin<DS1,LOSS>::Parameter para;
+	  opengm::learning::StructMaxMargin<DS1,LOSS> learner(dataset,para);
+	  
+	  
+	  INF::Parameter infPara;
+	  infPara.integerConstraint_ = true;
+	  learner.learn<INF>(infPara);
+	  
    }
+
+   //{
+	  //DS2 dataset(1);
+	  //std::cout << "Dataset includes " << dataset.getNumberOfModels() << " instances and has " << dataset.getNumberOfWeights() << " parameters."<<std::endl;
+	  
+	  
+	  //opengm::learning::StructMaxMargin<DS2,LOSS>::Parameter para;
+	  //opengm::learning::StructMaxMargin<DS2,LOSS> learner(dataset,para);
+	  
+	  
+	  //INF::Parameter infPara;
+	  //infPara.integerConstraint_ = true;
+	  //learner.learn<INF>(infPara);
+   //}
 
 
 }
